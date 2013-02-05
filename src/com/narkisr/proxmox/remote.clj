16e426d6146f29d9fdfd142df3689ec2951a7c6e
narkisr <narkisr@gmail.com>
2013-02-05 22:41:16 +0200

    clearer execption on login failure and retry addition

--- src/com/narkisr/proxmox/remote.clj
+++ src/com/narkisr/proxmox/remote.clj
@@ -1,33 +1,38 @@
 (ns com.narkisr.proxmox.remote
   (:require 
-    [taoensso.timbre :only (debug info error)]
     [cheshire.core :refer :all]
     [clj-http.client :as client]
     [clj-config.core :as conf])
-  (:use clojure.core.strint))
+  (:use clojure.core.strint
+        [taoensso.timbre :only (debug info error)])
+  (:import clojure.lang.ExceptionInfo)) 
 
 (def config (conf/read-config (<<  "~(System/getProperty \"user.home\")/.multistage.edn")))
 
 (def root (<< "https://~(-> config :hypervisor :host):8006/api2/json"))
 
 (defn retry
- "A retry for http calls against proxmox"
+  "A retry for http calls against proxmox, 
+  this is useful since some operations lock machine after completion"
   [ex try-count http-context]
- 
-  (println "Got:" ex)
-  (if (> try-count 4) false true))
+  (debug "re-trying due to " ex " attempt " try-count)
+  (.sleep (Thread/currentThread) 500)
+  (if (> try-count 2) false true))
 
 (defn root-post [api args]
-  (client/post (str root api) (assoc args :insecure? true)))
+  (client/post (str root api) (assoc args :insecure? true :retry-handler retry)))
 
 (defn root-delete [api args]
-  (client/delete (str root api) (assoc args :insecure? true)))
+  (client/delete (str root api) (assoc args :insecure? true :retry-handler retry)))
+
+(def login-creds (dissoc (assoc (config :hypervisor) :realm "pam") :host))
 
 (defn login []
-  (let [res (root-post "/access/ticket" {:form-params (dissoc (assoc (config :hypervisor) :realm "pam") :host)})]
-    (select-keys  
-      (:data (parse-string (:body res) true))   
-      [:CSRFPreventionToken :ticket])))
+  (try
+    (let [res (root-post "/access/ticket" {:form-params login-creds})]
+      (select-keys (:data (parse-string (:body res) true)) [:CSRFPreventionToken :ticket]))
+    (catch Exception e 
+      (throw (ExceptionInfo. "Failed to login" config)))))
 
 (def auth-headers
   (let [{:keys [CSRFPreventionToken ticket] :as headers} (login)]
@@ -35,8 +40,8 @@
       (assoc {} "Cookie" (str "PVEAuthCookie=" ticket)) "CSRFPreventionToken" CSRFPreventionToken)))
 
 (defn prox-post 
-  "Do a post against a proxmox instance with provided params with a retry count"
-  ([api] (prox-post api retry nil))
+  "A post against a proxmox instance with provided params"
+  ([api] (prox-post api nil))
   ([api params] 
    (if (nil? params)
      (root-post api {:headers auth-headers}) 
