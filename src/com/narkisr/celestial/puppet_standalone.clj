abd08a73bea38e28f21b702d1a8d57afb3f31276
narkisr <narkisr@gmail.com>
2013-02-07 17:53:31 +0200

    catching missing key auth issue

--- src/com/narkisr/celestial/puppet_standalone.clj
+++ src/com/narkisr/celestial/puppet_standalone.clj
@@ -1,16 +1,28 @@
 (ns com.narkisr.celestial.puppet-standalone
   "A standalone puppet provisioner"
+  (:import com.jcraft.jsch.JSchException)
   (:use 
     clojure.core.strint
     com.narkisr.celestial.core
     clj-ssh.ssh
+    [taoensso.timbre :only (debug info error warn)]
     [clojure.string :only (join)]
-    ))
+    [slingshot.slingshot :only  [throw+ try+]]))
 
+
+(def ssh-opts {:username "root" :strict-host-key-checking :no})
+
+(defn with-session [host f]
+  (let [session (session (ssh-agent {}) host ssh-opts)] 
+    (try+
+      (with-connection session (f session))
+      (catch #(= (.getMessage %) "Auth fail") e
+        (throw+ {:type ::auth :host host} "Failed to login make sure to ssh-copy-id to the remote host"))
+      )))
 
 (defn put [host file dest]
-  (let [agent (ssh-agent {}) session (session agent host {:username "root"}) ]
-    (with-connection session
+  (with-session host
+    (fn [session]
       (let [channel (ssh-sftp session)]
         (with-channel-connection channel
           (sftp channel {} :put file dest)
@@ -20,7 +32,7 @@
   (put (:host server) (str (module :src) (module :name) ".tar.gz")  "/tmp"))
 
 (defn execute [server & batch]
-  (let [agent (ssh-agent {}) session (session agent (server :host) {:username "root"}) ]
+  (let [agent (ssh-agent {}) session (session agent (server :host) ssh-opts) ]
     (with-connection session
       (let [result (ssh session {:in  (join "\n" batch ) })]
         (println result)))))
@@ -47,5 +59,5 @@
     ))
 
 #_(.apply 
-  (Standalone. {:host "192.168.5.203"} {:name "puppet-base-env" :src "/home/ronen/code/"}))
+    (Standalone. {:host "192.168.5.203"} {:name "puppet-base-env" :src "/home/ronen/code/"}))
 
