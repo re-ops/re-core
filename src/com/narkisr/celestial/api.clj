1191fecfda69aa13a42fe7dbabfcb217256492ef
narkisr <narkisr@gmail.com>
2013-02-08 03:17:00 +0200

    adding edn responses

--- src/com/narkisr/celestial/api.clj
+++ src/com/narkisr/celestial/api.clj
@@ -5,21 +5,25 @@
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(defn generate-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/edn"}
+   :body (pr-str data)})
+
 (defroutes app-routes
   (context "/provision" [] 
-           (defroutes provision-routes
-             (POST "/" [provision] 
-                   (debug "provisioning" provision)
-                   (puppetize provision)
-                   "Provision done"
-                   )))
+    (defroutes provision-routes
+       (POST "/" [provision] 
+         (debug "provisioning" provision)
+         (puppetize provision)
+         (generate-response {:status "pupptization done"}))))
   (context "/system" [] 
-           (defroutes system-routes
-             (POST "/:hypervisor" [hypervisor system]
-                  (debug "setting up system" system "on" hypervisor)
-                  (reload system hypervisor)
-                   "System reloaded"
-                   )))
+     (defroutes system-routes
+       (POST "/:hypervisor" [hypervisor system]
+         (debug "setting up system" system "on" hypervisor)
+         (reload system hypervisor)
+         (generate-response {:status "system is up"}))))
+
   (route/not-found "Not Found"))
 
 (def app
