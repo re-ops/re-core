ffb71a7175249da749532a8802dbae99d2a7c477
narkisr <narkisr@gmail.com>
2013-02-08 03:16:50 +0200

    indenting

--- src/com/narkisr/celestial/puppet_standalone.clj
+++ src/com/narkisr/celestial/puppet_standalone.clj
@@ -17,38 +17,27 @@
     (try+
       (with-connection session (f session))
       (catch #(= (.getMessage %) "Auth fail") e
-        (throw+ {:type ::auth :host host} "Failed to login make sure to ssh-copy-id to the remote host"))
-      )))
+        (throw+ {:type ::auth :host host} 
+                "Failed to login make sure to ssh-copy-id to the remote host")))))
 
 (defn put [host file dest]
   (with-session host
     (fn [session]
       (let [channel (ssh-sftp session)]
         (with-channel-connection channel
-          (sftp channel {} :put file dest)
-          )))))
+          (sftp channel {} :put file dest))))))
 
 (defn copy [server module]
   (put (:host server) (str (module :src) (module :name) ".tar.gz")  "/tmp"))
 
-#_(defn execute [{:keys [host]} & batch]
-  (with-session host 
+(defn execute [{:keys [host]} & batches]
+  (with-session host
     (fn [session]
-      (with-connection session
-        (let [{:keys [out exit] :as result} (ssh session {:in  (join "\n" batch ) })]
-          (if (= exit 0)
-            (debug out)
-            (throw+ (assoc result :type ::exec) "Failed to execute remotly")))))))
-
-(defn execute [server & batches]
-  (let [agent (ssh-agent {}) session (session agent (server :host) {:username "root"}) ]
-    (with-connection session
       (doseq [b batches]
         (let [{:keys [exit out] :as res} (ssh session {:in  (join "\n" b) })]
           (if (= exit 0)
             (debug out) 
-            (throw+ (merge res {:type ::provision-failed} (meta b)))
-            ))))))
+            (throw+ (merge res {:type ::provision-failed} (meta b)))))))))
 
 (defn step [n & steps] ^{:step n} steps)
 
@@ -59,9 +48,9 @@
     (use 'com.narkisr.celestial.core)
     (copy server module) 
     (execute server 
-       (step :extract "cd /tmp" (<< "tar -xzf ~(:name module).tar.gz")) 
-       (step :run (<< "cd /tmp/~(:name module)") "./run.sh")
-       (step :cleanup "cd /tmp" (<< "rm -rf ~(:name module)*"))) ))
+             (step :extract "cd /tmp" (<< "tar -xzf ~(:name module).tar.gz")) 
+             (step :run (<< "cd /tmp/~(:name module)") "./run.sh")
+             (step :cleanup "cd /tmp" (<< "rm -rf ~(:name module)*"))) ))
 
 
 #_(.apply-
