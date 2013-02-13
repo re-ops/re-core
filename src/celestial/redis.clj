6aa409d9e9f9b628d9a8f6187bf3d836e91a2903
narkisr <narkisr@gmail.com>
2013-02-13 18:51:40 +0200

    using a different locking scheme

--- src/celestial/redis.clj
+++ src/celestial/redis.clj
@@ -20,28 +20,54 @@
 
 (defn lock-id [k] (<< "lock.~{k}"))
 
-(defn lval [s] (Long/valueOf s))
+#_(defn lval [s] (Long/valueOf s))
 
-(defn try-aquire [lid timeout]
+#_(defn expired? [ts]
+   (<  (lval ts) (curr-time)))
+
+#_(defn try-aquire [lid timeout]
   (let [ts (wcar (car/getset lid (timestamp timeout)))]
-    (if (<  (lval ts) (curr-time)) true false )))
+    (if (expired? ts)  true false)))
 
-(defn- check-validity [lid timeout]
+#_(defn- check-expired [lid timeout]
   "A lock already exists we check if it had a valid timestamp"
-  (let [ts (wcar (car/get lid))]
-    (if (< (lval ts) (curr-time)); its not valid
-      (try-aquire lid timeout) false )))
+    (if (expired? (wcar (car/get lid)))
+      (try-aquire lid timeout) false ))
 
-(defn lock [k timeout]
+#_(defn acquire_lock  [k timeout]
   "Locks a key k with expiry of timeout (mili), return true if lock successful
    see http://redis.io/commands/setnx"
   (let [lid (lock-id k)  res (wcar (car/setnx lid (timestamp timeout)))]
     (if (= res 1) 
       true
-      (check-validity lid timeout)
+      (check-expired lid timeout)
       ))) 
 
-(defn release [k] (car/del (lock-id k)))
+(defn- gen-uuid [] (str (java.util.UUID/randomUUID)))
 
-;(lock 5 (* 1000 60))
+(defn acquire [id & [{:keys [wait-time expiry] :or {wait-time 2000 expiry 2}} & _]]
+  "Acquires lock, returns lock unique id if successful 
+   wait-time (mili) wait time for lock,
+   expiry (sec) how long the lock is valid 
+   see http://dr-josiah.blogspot.co.il/2012/01/creating-lock-with-redis.html "
+  (let [lid (lock-id id) uuid (gen-uuid)
+        wait (+ (curr-time) wait-time)]
+    (loop []
+      (if (< wait (curr-time))
+        false
+        (if (= 1 (wcar (car/setnx lid uuid)))
+          (do (wcar (car/expire lid expiry)) uuid)
+          (do 
+            (when (< (wcar (car/ttl lid)) 0)
+              (wcar (car/expire lid expiry)))
+            (recur))))))) 
+
+
+(defn release [id uuid] 
+  (let [lid (lock-id id)]
+    (wcar (car/watch lid)) 
+    (if (= (wcar (car/get lid)) uuid)
+      (do (wcar (car/del lid)) true)
+      (do (wcar (car/unwatch)) false))))
+
 ;(wcar (car/get (lock-id 5)))
--- test/celestial/test/redis.clj
+++ test/celestial/test/redis.clj
@@ -2,15 +2,23 @@
   "Redis test assume a redis sandbox on localhost, use https://github.com/narkisr/redis-sandbox"
   (:use 
     clojure.test 
-    [celestial.redis :only (lock release)]))
+    [celestial.redis :only (acquire release)]))
 
 
 (deftest ^:redis basic-locking 
-  (is (lock 1 200)) 
-  (is (not (lock 1 200))) 
-  (Thread/sleep 200)
-  (is (lock 1 200)))
-
+  (is (acquire 2)) ; for 2 sec
+  (is (not (acquire 2 {:wait-time 200}))) ; too early
+  (Thread/sleep 1000)
+  (is (acquire 2)))
 
 (deftest ^:redis releasing  
-  )
+  (let [uuid (acquire 3)]
+    (is (not (acquire 3 {:wait-time 200}))) ; too early
+    (is (release 3 uuid)) 
+    (is (acquire 3))))
+
+(deftest ^:redis aleady-released
+  (let [uuid (acquire 4 {:wait-time 2000}) f1 (future (release 4 uuid))]
+     (Thread/sleep 200); letting future run 
+     (is (not (release 4 uuid)))
+    ))
