ebecf918a6c066407e831d6d6832d5eff3be9938
narkisr <narkisr@gmail.com>
2013-02-18 04:53:16 +0200

    addign in memory db

--- src/celestial/persistency.clj
+++ src/celestial/persistency.clj
@@ -1,2 +1,11 @@
 (ns celestial.persistency
  )
+
+(def hosts 
+  {:redis-test.local {:classes {:redis {:append true}}}
+   :redis-test.home.net {:classes {:redis {:append true}}}
+   :redis {:classes {:redis {:append true}}}
+   })
+
+(defn profile [host]
+  (hosts host))
