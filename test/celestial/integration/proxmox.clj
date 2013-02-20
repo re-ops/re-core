b0a1af22d1527899311f5323be2935c052fd8aff
narkisr <narkisr@gmail.com>
2013-02-20 22:35:52 +0200

    using latest spec

--- test/celestial/integration/proxmox.clj
+++ test/celestial/integration/proxmox.clj
@@ -2,30 +2,27 @@
   "Integration tests assume a proxmox vm with local address make sure to configure it"
   (:use clojure.test 
         proxmox.provider
-        [celestial.common :only (config)]
+        [celestial.common :only (config slurp-edn)]
         )
   (:import 
     [proxmox.provider Container]))
 
-(def spec 
-  {:vmid 203 :ostemplate  "local:vztmpl/ubuntu-12.04-puppet_3-x86_64.tar.gz"
-   :cpus  4 :memory  4096 :hostname  "foobar" :disk 30
-   :ip_address  "192.168.1.30" :password "foobar1" :hypervisor "proxmox"})
+(def spec (slurp-edn "fixtures/redis-system.edn"))
 
 (def local-prox 
-  {:hypervisor  {:username "root" :password "foobar" :host "localhost" :ssh-port 22222}})
+  {:hypervisor {:username "root" :password "foobar" :host "localhost" :ssh-port 22222}})
 
-(def fake-id (assoc spec :vmid 190))
+(def fake-id (update-in spec [:machine :vmid] (fn [o] 190)))
 
 (alter-var-root (var config) (fn [old] local-prox))
 
 (deftest ^:proxmox non-existing 
-  (let [{:keys [hypervisor]} fake-id  ct (Container. hypervisor fake-id)]
+  (let [{:keys [machine]} fake-id  ct (Container. (machine :hypervisor) machine)]
     (.stop ct)
     (.delete ct)))
 
 (deftest ^:proxmox full-cycle
-  (let [{:keys [hypervisor]} spec  ct (Container. hypervisor spec)]
+  (let [{:keys [machine]} spec ct (Container. (machine :hypervisor) machine)]
     (.stop ct)
     (.delete ct) 
     (.create ct) 
