3b055c484b9463a77fdd3ab177192969b518e402
narkisr <narkisr@gmail.com>
2013-03-01 06:03:14 +0200

    ignoring exit code for restart

--- src/celestial/dnsmasq.clj
+++ src/celestial/dnsmasq.clj
@@ -5,20 +5,22 @@
     [clojure.core.strint :only (<<)]
     [celestial.ssh :only (execute step)]))
 
-(def restart (step :restart-service "sudo service dnsmasq restart"))
+(defn ignore-code [s]
+  (with-meta s (merge (meta s) {:ignore-code true})))
+
+(def restart 
+  (step :restart-service "sudo service dnsmasq stop" "sudo service dnsmasq start"))
 
 (defn add-host [{:keys [hostname ip_address dnsmasq]}]
   (execute dnsmasq 
-      (step :add-host (<< "echo '~{ip_address} ~{hostname}' | sudo tee -a /etc/hosts >>/dev/null" ))
-       restart 
-      ))
+      (step :add-host (<< "echo '~{ip_address} ~{hostname}' | sudo tee -a /etc/hosts >> /dev/null" ))
+      (ignore-code restart)))
 
 
 (defn remove-host [{:keys [hostname ip_address dnsmasq]}]
   (execute dnsmasq 
     (step :remove-host (<< "sudo sed -ie \"\\|^~{ip_address} ~{hostname}\\$|d\" /etc/hosts"))
-     restart
-        ))
+    (ignore-code restart)))
 
 ; (add-host "foo" "192.168.20.90")
 ; (remove-host "foo" "192.168.20.90")
