b43efc596ef00665cda57a866f6ea268987155ec
narkisr <narkisr@gmail.com>
2013-03-04 00:48:52 +0200

    enabling puppet integration testing

--- test/celestial/integration/puppet.clj
+++ test/celestial/integration/puppet.clj
@@ -1,16 +1,21 @@
 (ns celestial.integration.puppet
   "Requires both proxmox redis and a celetial instance to reply to puppet ext queries"
+  (:require 
+    [celestial.persistency :as p])
   (:use 
     clojure.test    
     [celestial.api :only (app)]
     [ring.adapter.jetty :only (run-jetty)] 
     [celestial.tasks :only (reload puppetize)]
     [celestial.common :only (config)]
-    [celestial.fixtures :only (spec local-prox type)]
+    [celestial.fixtures :only (spec local-prox redis-type)]
     [celestial.puppet-standalone :only (copy-module)]))
 
 
-#_(deftest ^:puppet redis-provision
+(deftest ^:puppet redis-provision
   (with-redefs [config local-prox]
     (reload spec) 
-    (puppetize (:provision baseline))))
+    (p/new-type "red1" redis-type) 
+    (p/register-host "red1" "redis" spec) 
+    (run-jetty app {:port 8080 :join? false})
+    (puppetize redis-type spec)))
