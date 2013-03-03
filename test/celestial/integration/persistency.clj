74794cfb8caabebf1a749bba378be3b0b4428b33
narkisr <narkisr@gmail.com>
2013-03-04 00:49:36 +0200

    using redis-type

--- test/celestial/integration/persistency.clj
+++ test/celestial/integration/persistency.clj
@@ -4,21 +4,21 @@
   (:require 
     [celestial.persistency :as p])
   (:use clojure.test
-    [celestial.fixtures :only (spec type)]
+    [celestial.fixtures :only (spec redis-type)]
     [celestial.redis :only (clear-all)]))
 
 
 (deftest ^:redis sanity
     (clear-all)
-    (p/new-type "redis" type) 
+    (p/new-type "redis" redis-type) 
     (p/register-host "redis.local" "redis" spec) 
-    (is (= (p/type "redis") type))
+    (is (= (p/type "redis") redis-type))
     (is (= (p/host "redis.local") {:machine spec :type "redis"}))
     )
 
 (deftest ^:redis fuzzy-lookup 
     (clear-all)
-    (p/new-type "redis" type) 
+    (p/new-type "redis" redis-type) 
     (p/register-host "redis" "redis" spec) 
     (is (= (p/fuzzy-host "redis.local") {:machine spec :type "redis"})))
 
