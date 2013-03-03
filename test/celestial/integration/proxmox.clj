c1dd0461775994cf1bc4d53fe934e3107c666b9a
narkisr <narkisr@gmail.com>
2013-03-03 07:03:35 +0200

    using with-redefs instead of global alter root

--- test/celestial/integration/proxmox.clj
+++ test/celestial/integration/proxmox.clj
@@ -13,18 +13,19 @@
 
 (def fake-id (update-in spec [:proxmox :vmid] (fn [o] 190)))
 
-(alter-var-root (var config) (fn [old] local-prox))
 
 (deftest ^:proxmox non-existing 
-  (let [ct (construct fake-id) ]
+  (with-redefs [config local-prox]
+    (let [ct (construct fake-id) ]
     (.stop ct)
-    (.delete ct)))
+    (.delete ct))))
 
 (deftest ^:proxmox full-cycle
-  (let [ct (construct spec )]
+  (with-redefs [config local-prox]
+   (let [ct (construct spec )]
     (.stop ct)
     (.delete ct) 
     (.create ct) 
     (.start ct)
-    (is (= (.status ct) "running"))))
+    (is (= (.status ct) "running")))))
 
