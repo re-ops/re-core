0b7e09a38d0e8791b0caa9dd9a7164be55387fdd
narkisr <narkisr@gmail.com>
2013-03-04 00:50:35 +0200

    using vconstruct

--- test/celestial/test/proxmox.clj
+++ test/celestial/test/proxmox.clj
@@ -3,7 +3,7 @@
     [proxmox.remote :only (prox-get)]
     [proxmox.provider :only (vzctl enable-features ->Container)]
     [celestial.common :only (config)]
-    [celestial.model :only (create-vm)]
+    [celestial.model :only (vconstruct)]
     [celestial.fixtures :only (spec local-prox)]
     expectations.scenarios) 
   (:import 
@@ -12,15 +12,15 @@
 (let [{:keys [machine proxmox]} spec]
   (scenario 
     (expect java.lang.AssertionError 
-       (create-vm (assoc-in spec [:proxmox :vmid] nil)))
+       (vconstruct (assoc-in spec [:proxmox :vmid] nil)))
     (expect java.lang.AssertionError 
-       (create-vm (assoc-in spec [:proxmox :vmid] "string") ))
+       (vconstruct (assoc-in spec [:proxmox :vmid] "string") ))
     ))
 
 
 (with-redefs [config local-prox]
   (def ct 
-    (create-vm (assoc-in spec [:proxmox :features] ["nfs:on"]))))
+    (vconstruct (assoc-in spec [:proxmox :features] ["nfs:on"]))))
 
 (scenario 
   (enable-features ct) 
