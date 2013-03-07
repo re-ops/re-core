e914e52cc5e00a2ae91e96f023ab5b5c9dec3c74
narkisr <narkisr@gmail.com>
2013-03-07 03:07:30 +0200

    moving to clojure 1.5 carmine 1.6 and latest ring

--- project.clj
+++ project.clj
@@ -1,39 +1,40 @@
 (defproject celestial "0.0.1"
   :description "A launching pad for virtualized applications"
-  :url ""
-  :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "TBD"
+  :license {:name "Apache V2" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
 
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.cli "0.2.1" :exclusions [org.clojure/clojure]]
-                 [clj-ssh "0.5.0" :exclusions [org.clojure/clojure]  ]
-                 [clj-config "0.2.0" :exclusions [org.clojure/clojure] ]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/tools.cli "0.2.1" ]
+                 [clj-ssh "0.5.0" ]
+                 [clj-config "0.2.0" ]
                  [prismatic/plumbing "0.0.1"]
                  [cheshire "5.0.1"]
-                 [com.taoensso/timbre "1.4.0" :exclusions [org.clojure/clojure]]
-                 [narkisr/closchema "0.3.0-SNAPSHOT"]
+                 [com.taoensso/timbre "1.4.0" ]
                  [org.clojure/core.incubator "0.1.2"]
-                 [slingshot "0.10.3" :exclusions [org.clojure/clojure]]
+                 [slingshot "0.10.3" ]
                  [clj-http "0.6.4"]
                  [clj-yaml "0.4.0"]
-                 [compojure "1.1.1"]
-                 [ring "1.0.2"]
-                 [org.clojure/data.json "0.2.1" :exclusions [org.clojure/clojure]]
-                 [com.taoensso/carmine "1.5.0"]
+                 [org.clojure/data.json "0.2.1" ]
+                 [com.taoensso/carmine "1.6.0"]
                  [org.clojure/core.memoize "0.5.2"]
                  [metrics-clojure "0.9.2"]
-                 [clj-aws-ec2 "0.2.0"]
+                 [clj-aws-ec2 "0.2.0" :exclusions  [org.codehaus.jackson/jackson-core-asl]]
                  [trammel "0.7.0"]
                  [mississippi "1.0.1"]
                  [metrics-clojure-ring "0.9.2"]
-                 [ring/ring-jetty-adapter "1.1.6"]
+                 [ring "1.1.8"]
+                 [compojure "1.1.5" :exclusions  [ring/ring-core]]
+                 [ring/ring-jetty-adapter "1.1.8"]
                  [ring-middleware-format "0.2.4"]]
 
-  :plugins  [[lein-autoexpect "0.2.5"]  [jonase/eastwood "0.0.2"] [lein-tarsier "0.10.0"]
-             [lein-ring "0.7.3"] [lein-expectations "0.0.7"] #_[lein-checkouts "1.1.0"]]
+  :exclusions [org.clojure/clojure]
 
-  :profiles {
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]} 
-             :dev {:dependencies [[ring-mock "0.1.3"] [expectations "1.4.24"] [junit/junit "4.8.1"] ]}}
+  :plugins  [[lein-autoexpect "0.2.5"]  [jonase/eastwood "0.0.2"] [lein-tarsier "0.10.0"]
+             [lein-expectations "0.0.7"] #_[lein-checkouts "1.1.0"]
+             [lein-pedantic "0.0.5"] 
+             ]
+
+  :profiles {:dev {:dependencies [[ring-mock "0.1.3"] [expectations "1.4.24"] [junit/junit "4.8.1"] ]}}
                  
 
   :aot [proxmox.provider celestial.puppet-standalone celestial.api]
