68754cc2ac1d48a1bd5b46cb41e73f3021b49d96
narkisr <narkisr@gmail.com>
2013-03-07 06:43:41 +0200

    clearing print

--- test/celestial/test/persistency.clj
+++ test/celestial/test/persistency.clj
@@ -10,9 +10,9 @@
 
 
 (scenario 
-  (with-redefs [car/hgetall* (fn [_] (println "here") {:k "v"}) wcar-disable true]
+  (with-redefs [car/hgetall* (fn [_] {:k "v"}) wcar-disable true]
     (expect {:k "v"} (p/host "bar"))))
 
 (scenario 
-  (with-redefs [car/hgetall* (fn [_] nil) wcar-disable true ]
+  (with-redefs [car/hgetall* (fn [_] nil) wcar-disable true]
     (expect clojure.lang.ExceptionInfo (p/host "bar"))))
