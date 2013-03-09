149b418baf1fd939adde54216139991584e8bb8f
narkisr <narkisr@gmail.com>
2013-03-09 06:44:04 +0200

    implemeting status wait

--- src/aws/provider.clj
+++ src/aws/provider.clj
@@ -1,5 +1,6 @@
 (ns aws.provider
   (:require [aws.sdk.ec2 :as ec2])
+  (:import java.util.UUID)
   (:use 
     [trammel.core :only  (defconstrainedrecord)]
     [celestial.provider :only (str? vec?)]
@@ -23,31 +24,43 @@
 
 (def ids (synched-map :aws-keys))
 
+(defn wait-for-status [s instance & {:keys [timeout] :or {timeout }}]
+ (while (not (= status (.status Instance)))
+    
+   ) 
+  )
+
 (defconstrainedrecord Instance [endpoint conf uuid]
   "An Ec2 instance, uuid used for instance-id tracking"
-  [(empty? (:errors (validate conf instance-valid))) (not (nil? endpoint))]
+  [(empty? (:errors (validate conf instance-valid))) 
+   (not (nil? endpoint)) (instance? UUID uuid)]
   Vm
   (create [this] 
-          (debug "creating" conf)
-          (debug (ec2/run-instances (creds) conf)))
+      (debug "creating" conf)
+      (swap! ids assoc uuid (-> (ec2/run-instances (creds) conf) :instances first :id)) 
+      this)
   (delete [this]
-          (debug "deleting"))
+          (debug "deleting" uuid)
+          (ec2/terminate-instances (creds) (@ids uuid))
+          )
   (start [this]
-         (debug "starting" )
-         (ec2/start-instances (creds)  "i-8843dec2"))
+         (debug "starting" uuid)
+         (ec2/start-instances (creds) (@ids uuid)))
   (stop [this]
         (debug "stopping" )
-        (ec2/stop-instances (creds)  "i-8843dec2"))
+        (ec2/stop-instances (creds)  (@ids uuid)))
   (status [this] 
-          (ec2/describe-instances (creds)))) 
+      (-> (ec2/describe-instances (creds) (ec2/instance-id-filter (@ids uuid)))
+          first :instances first :state :name
+          ))) 
 
 (defmethod translate :aws [{:keys [aws machine]}] 
-   [(dissoc aws :endpoint) (java.util.UUID/randomUUID) (aws :endpoint) ] )
+  [(aws :endpoint) (dissoc aws :endpoint) (UUID/randomUUID)])
 
 (defmethod vconstruct :aws [{:keys [aws] :as spec}]
-  (let [[conf endpoint uuid] (translate spec)]
-    (->Instance endpoint conf uuid)))
+    (apply ->Instance (translate spec)))
 
-; (use 'celestial.fixtures)
-; (.create (vconstruct redis-ec2-spec))
+; (def m (.create (vconstruct celestial.fixtures/redis-ec2-spec)))
+
+; (.status m)
 
