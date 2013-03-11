e0275a847dfd134e42b6818e18f06f37f297a2d2
narkisr <narkisr@gmail.com>
2013-03-11 17:14:58 +0200

    using ec2 macro and implementing wait for status

--- src/aws/provider.clj
+++ src/aws/provider.clj
@@ -1,12 +1,15 @@
 (ns aws.provider
   (:require [aws.sdk.ec2 :as ec2])
-  (:import java.util.UUID)
+  (:import (java.util UUID))
   (:use 
-    [trammel.core :only  (defconstrainedrecord)]
+    [aws.sdk.ec2 :only 
+       (run-instances describe-instances terminate-instances start-instances
+        stop-instances instance-filter instance-id-filter)]
+    [trammel.core :only (defconstrainedrecord)]
     [celestial.provider :only (str? vec?)]
     [celestial.redis :only (synched-map)]
     [celestial.core :only (Vm)]
-    [celestial.common :only (config import-logging)]
+    [celestial.common :only (config import-logging curr-time)]
     [mississippi.core :only (required numeric validate)]
     [celestial.model :only (translate vconstruct)]))
 
@@ -25,10 +28,16 @@
 (def ids (synched-map :aws-keys))
 
 (defn wait-for-status [req-stat instance & {:keys [timeout] :or {timeout 3000}}]
- (while (not (= req-stat (.status instance)))
-    
-   ) 
-  )
+  (let [wait (+ (curr-time) timeout)]
+    (loop []
+      (if (> wait (curr-time))
+        (if (= req-stat (.status instance)) 
+          true
+          (recur)) 
+        false))))
+
+(defmacro ec2 [f & args]
+ `(~f (assoc (creds) :endpoint ~'endpoint) ~@args))
 
 (defconstrainedrecord Instance [endpoint conf uuid]
   "An Ec2 instance, uuid used for instance-id tracking"
@@ -36,31 +45,31 @@
    (not (nil? endpoint)) (instance? UUID uuid)]
   Vm
   (create [this] 
-      (debug "creating" conf)
-      (swap! ids assoc uuid (-> (ec2/run-instances (creds) conf) :instances first :id)) 
-      this)
+          (debug "creating" uuid)
+          (swap! ids assoc uuid (-> (ec2 run-instances conf) :instances first :id)) 
+          this)
   (delete [this]
           (debug "deleting" uuid)
-          (ec2/terminate-instances (creds) (@ids uuid))
-          )
+          (ec2 terminate-instances (@ids uuid)))
   (start [this]
          (debug "starting" uuid)
-         (ec2/start-instances (creds) (@ids uuid)))
+         (ec2 start-instances (@ids uuid)))
   (stop [this]
-        (debug "stopping" )
-        (ec2/stop-instances (creds)  (@ids uuid)))
+        (debug "stopping" uuid )
+        (stop-instances  (@ids uuid)))
   (status [this] 
-      (-> (ec2/describe-instances (creds) (ec2/instance-id-filter (@ids uuid)))
-          first :instances first :state :name
-          ))) 
+          (-> (ec2 describe-instances (instance-id-filter (@ids uuid)))
+              first :instances first :state :name
+              ))) 
 
 (defmethod translate :aws [{:keys [aws machine]}] 
   [(aws :endpoint) (dissoc aws :endpoint) (UUID/randomUUID)])
 
 (defmethod vconstruct :aws [{:keys [aws] :as spec}]
-    (apply ->Instance (translate spec)))
+  (apply ->Instance (translate spec)))
 
-; (def m (.create (vconstruct celestial.fixtures/redis-ec2-spec)))
-
-; (.status m)
+(comment 
+  (def m (.create (vconstruct celestial.fixtures/redis-ec2-spec))) 
+  (.status m) 
+  (.delete m)) 
 
