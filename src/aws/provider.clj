5bfc3e76414e933e9862f970e5308f4799c44eba
narkisr <narkisr@gmail.com>
2013-03-11 18:27:06 +0200

    using mindingbender for time units

--- src/aws/provider.clj
+++ src/aws/provider.clj
@@ -2,6 +2,8 @@
   (:require [aws.sdk.ec2 :as ec2])
   (:import (java.util UUID))
   (:use 
+    [minderbinder.time :only (parse-time-unit)]
+    [slingshot.slingshot :only  [throw+ try+]]
     [aws.sdk.ec2 :only 
        (run-instances describe-instances terminate-instances start-instances
         stop-instances instance-filter instance-id-filter)]
@@ -27,14 +29,15 @@
 
 (def ids (synched-map :aws-keys))
 
-(defn wait-for-status [req-stat instance & {:keys [timeout] :or {timeout 3000}}]
-  (let [wait (+ (curr-time) timeout)]
+(defn wait-for-status [instance req-stat  {:keys [timeout] :or {timeout [5 :minute]}}]
+  "Waiting for ec2 machine status timeout is in mili"
+  (let [wait (+ (curr-time) (parse-time-unit timeout))]
     (loop []
       (if (> wait (curr-time))
         (if (= req-stat (.status instance)) 
           true
-          (recur)) 
-        false))))
+          (do (Thread/sleep 2000) (recur))) 
+        (throw+ {:type ::aws:status-failed :message "Failed to wait for status" :status req-stat})))))
 
 (defmacro ec2 [f & args]
  `(~f (assoc (creds) :endpoint ~'endpoint) ~@args))
@@ -50,13 +53,17 @@
           this)
   (delete [this]
           (debug "deleting" uuid)
-          (ec2 terminate-instances (@ids uuid)))
+          (ec2 terminate-instances (@ids uuid))
+          (wait-for-status this "terminated" {:timeout [5 :minute]}) 
+          (swap! ids dissoc uuid))
   (start [this]
          (debug "starting" uuid)
-         (ec2 start-instances (@ids uuid)))
+         (ec2 start-instances (@ids uuid))
+         (wait-for-status this "running" {:timeout [5 :minute]}))
   (stop [this]
         (debug "stopping" uuid )
-        (stop-instances  (@ids uuid)))
+        (ec2 stop-instances  (@ids uuid))
+        (wait-for-status this "stopped" {:timeout [2 :minute]}))
   (status [this] 
           (-> (ec2 describe-instances (instance-id-filter (@ids uuid)))
               first :instances first :state :name
@@ -69,7 +76,15 @@
   (apply ->Instance (translate spec)))
 
 (comment 
+  (use 'celestial.fixtures)
   (def m (.create (vconstruct celestial.fixtures/redis-ec2-spec))) 
+  (.start m)
   (.status m) 
-  (.delete m)) 
+  (.stop m) 
+  (wait-for-status m "stopped") 
+  (.delete m)
+  (parse-time-unit [1 :minute])
+  (wait-for-status m "running" {:timeout [1 :minute]}) 
+  ) 
+
 
