8e8689e4c635f46d4def5754b77f1d612d065472
narkisr <narkisr@gmail.com>
2013-03-14 02:07:12 +0200

    aws unit test

--- test/celestial/test/aws.clj
+++ test/celestial/test/aws.clj
@@ -1,11 +1,13 @@
 (ns celestial.test.aws
   (:use 
     [celestial.fixtures :only (redis-ec2-spec)]
-    expectations
+    expectations.scenarios
     aws.provider
     [celestial.model :only (vconstruct)]
     ))
 
-(let [instance (vconstruct redis-ec2-spec)] 
-  (expect clojure.lang.ExceptionInfo (.start instance)))
+(scenario 
+  (with-redefs [ids (fn [] (atom {}))]
+    (let [instance (vconstruct redis-ec2-spec)] 
+      (expect clojure.lang.ExceptionInfo (.start instance)))))
 
