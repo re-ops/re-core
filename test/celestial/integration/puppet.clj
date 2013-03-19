aee92bd7a924a4da7340fc43e6feea572ab5b64f
narkisr <narkisr@gmail.com>
2013-03-20 00:53:53 +0200

    adding common cycle fn

--- test/celestial/integration/puppet.clj
+++ test/celestial/integration/puppet.clj
@@ -4,28 +4,30 @@
     [celestial.persistency :as p])
   (:use 
     clojure.test    
+    [taoensso.timbre :only (debug info error warn)]
     [celestial.api :only (app)]
     [ring.adapter.jetty :only (run-jetty)] 
     [celestial.tasks :only (reload puppetize)]
     [celestial.common :only (config)]
     [celestial.redis :only (clear-all)]
     [celestial.persistency :only (host register-host new-type)]  
-    [celestial.fixtures :only (redis-prox-spec redis-ec2-spec local-prox redis-type)]
-    [celestial.puppet-standalone :only (copy-module)]))
+    [celestial.fixtures :only (redis-prox-spec redis-ec2-spec local-prox redis-type)]))
+
+(defn run-cycle [spec type]
+  (let [hostname (get-in redis-ec2-spec [:machine :hostname])]
+    (clear-all) 
+    (new-type "redis" type) 
+    (register-host spec) 
+    (let [vm (reload spec)] 
+      (puppetize type (host hostname))
+      (.delete vm))))
 
 (deftest ^:puppet redis-ec2-provision
   "assumes a working ec2 defs in ~/.celestial.edn"
-  (let [puppet-ami (assoc-in redis-ec2-spec [:aws :image-id] "ami-4eb1ba3a")
-        hostname (get-in redis-ec2-spec [:machine :hostname])]
-    (clear-all)
-    (new-type "redis" redis-type)
-    (register-host redis-ec2-spec)
-    (reload puppet-ami) 
-    (puppetize redis-type (host hostname))))
+  (let [puppet-ami (assoc-in redis-ec2-spec [:aws :image-id] "ami-4eb1ba3a")]
+    (run-cycle puppet-ami redis-type)))
 
-#_(deftest ^:puppet redis-prox-provision
-  (with-redefs [config local-prox]
-    (reload redis-prox-spec) 
-    (puppetize redis-type redis-prox-spec)))
+(deftest ^:puppet redis-prox-provision
+    (run-cycle redis-prox-spec redis-type))
 
 
