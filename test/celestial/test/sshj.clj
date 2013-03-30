54ab340d1a95dbfa16ffca4da905cf44ac78ceee
narkisr <narkisr@gmail.com>
2013-03-31 01:55:05 +0300

    clearing

--- test/celestial/test/sshj.clj
+++ test/celestial/test/sshj.clj
@@ -2,21 +2,3 @@
   (:use celestial.sshj clojure.test)
  )
 
-(def tasks
-  { :download {
-      :wget "wget,,," :tar "tar -xvzf"
-     }
-     :puppet ^{:depends #{:download :yml-up}} {
-        :cd "cd /tmp/~{module}" :run "./scripts/run"
-     } 
-     :yml-up ^{:depends #{:download}}{
-        :cd "cd /tmp/~{module}" :run "./scripts/run"
-     }
-     :clean ^:last {
-      :rm "rm -rf /tmp/~{module}"
-     }
-   }
-  )
-
-(deftest deps
-  (is (= (sorted-deps tasks) [:download :yml-up :puppet :clean])))
