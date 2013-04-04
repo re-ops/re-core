cbc2a94b7398f28f801c6552fc2be2a41156e0bb
narkisr <narkisr@gmail.com>
2013-04-04 14:54:36 +0300

    spacing

--- src/celestial/api.clj
+++ src/celestial/api.clj
@@ -8,6 +8,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.)
+
 (ns celestial.api
   (:refer-clojure :exclude [type])
   (:use [compojure.core :only (defroutes context POST GET routes)] 
