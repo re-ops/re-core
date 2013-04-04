1182c12b8fb8ccaed8ea4de54bf184af04f1d76e
narkisr <narkisr@gmail.com>
2013-04-04 13:59:36 +0300

    adding missing files

--- src/celestial/api.clj
+++ src/celestial/api.clj
@@ -1,7 +1,7 @@
 (comment 
    Celestial, Copyright 2012 Ronen Narkis, narkisr.com
    Licensed under the Apache License,
-   Version 2.0  (the "License" you may not use this file except in) compliance with the License.
+   Version 2.0  (the "License") you may not use this file except in compliance with the License.
    You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
