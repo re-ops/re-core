b2c44a17fe1778d9453af8911cb370282f512604
narkisr <narkisr@gmail.com>
2013-04-06 18:06:31 +0300

    Configuration validation works

--- src/celestial/config.clj
+++ src/celestial/config.clj
@@ -28,13 +28,13 @@
   [c] (map? c))
 
 (defvalidator str-v
-  {:default-message-format "%s must be a hash"}
+  {:default-message-format "%s must be a string"}
   [c] (string? c))
 
 (defmacro validate-nest 
   "Bouncer nested maps validation with prefix key"
   [target pref & body]
-  (let [with-prefix (reduce (fn [r [ks vs]] (cons (into [pref] ks) (cons vs  r))) '() (partition 2 body))]
+  (let [with-prefix (reduce (fn [r [ks vs]] (cons (into pref ks) (cons vs  r))) '() (partition 2 body))]
   `(b/validate ~target ~@with-prefix)))
 
 (defn base-v [c]
@@ -45,7 +45,7 @@
 (defn celestial-v
   "Base config validation"
   [c]
-  (validate-nest c :celestial
+  (validate-nest c [:celestial]
     [:port] [v/required v/number]
     [:https-port] [v/required v/number]
     [:log :level] [v/required (v/member levels :message (<< "log level must be either ~{levels}"))]
@@ -56,16 +56,16 @@
 (defn proxmox-v 
   "proxmox section validation"
   [c]
-  (validate-nest c [:hypervizor :proxmox]
+  (validate-nest c [:hypervisor :proxmox]
     [:username] [v/required str-v]
     [:password] [v/required str-v]
     [:host] [v/required str-v]
-    [:ssh-port] [v/required str-v]))
+    [:ssh-port] [v/required v/number]))
 
 (defn aws-v 
   "proxmox section validation"
   [c]
-  (validate-nest c [:hypervizor :aws]
+  (validate-nest c [:hypervisor :aws]
     [:access-key] [v/required str-v]
     [:secret-key] [v/required str-v]
     [:endpoint] [v/required str-v]))
@@ -74,8 +74,8 @@
   "applies all validations on a configration map"
   [c]
   (cond-> (-> c celestial-v second base-v second)
-    (get-in c [:hypervizor :proxmox]) ((comp second proxmox-v)) 
-    (get-in c [:hypervizor :aws]) ((comp second aws-v)) 
+    (get-in c [:hypervisor :proxmox]) (-> proxmox-v second) 
+    (get-in c [:hypervisor :aws])  (-> aws-v second)
     ))
 
 (def config-paths
---
+++ test/celestial/test/config.clj
@@ -0,0 +1,22 @@
+(ns celestial.test.config
+  (:use 
+    midje.sweet
+    [celestial.fixtures :only (local-prox)]
+    [flatland.useful.map :only (dissoc-in*)]
+    [celestial.config :only (validate-conf proxmox-v)]))
+
+(defn validate-missing [& ks]
+ (-> (validate-conf (dissoc-in* local-prox ks)) :bouncer.core/errors vals))
+
+(fact "legal configuration"
+     (:bouncer.core/errors (validate-conf local-prox))  => nil)
+
+(fact "missing celestial options detected"
+     (validate-missing :celestial :https-port) => (contains {:https-port '("https-port must be present")})  
+     (validate-missing :celestial :port) => (contains {:port '("port must be present")}))
+
+(fact "missing proxmox options"
+      (validate-missing :hypervisor :proxmox :password) =>
+      (contains {:proxmox {:password '("password must be present")}} ))
+
+
