4321dfbda7e0a90837d4f56ee95979fef3748bc6
narkisr <narkisr@gmail.com>
2013-04-06 18:15:42 +0300

    Aws options

--- test/celestial/test/config.clj
+++ test/celestial/test/config.clj
@@ -6,17 +6,22 @@
     [celestial.config :only (validate-conf proxmox-v)]))
 
 (defn validate-missing [& ks]
- (-> (validate-conf (dissoc-in* local-prox ks)) :bouncer.core/errors vals))
+  (-> (validate-conf (dissoc-in* local-prox ks)) :bouncer.core/errors vals))
 
 (fact "legal configuration"
-     (:bouncer.core/errors (validate-conf local-prox))  => nil)
+      (:bouncer.core/errors (validate-conf local-prox))  => nil)
 
 (fact "missing celestial options detected"
-     (validate-missing :celestial :https-port) => (contains {:https-port '("https-port must be present")})  
-     (validate-missing :celestial :port) => (contains {:port '("port must be present")}))
+      (validate-missing :celestial :https-port) => (contains {:https-port '("https-port must be present")})  
+      (validate-missing :celestial :port) => (contains {:port '("port must be present")}))
 
 (fact "missing proxmox options"
       (validate-missing :hypervisor :proxmox :password) =>
       (contains {:proxmox {:password '("password must be present")}} ))
 
 
+(fact "missing aws options"
+      (get-in 
+        (validate-conf 
+          (assoc-in local-prox [:hypervisor :aws] {})) [:bouncer.core/errors :hypervisor :aws]) =>
+      (contains {:access-key '("access-key must be present")}))
