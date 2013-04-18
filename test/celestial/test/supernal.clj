1a804ecd01755fe6af922c965113a348acd1fa85
narkisr <narkisr@gmail.com>
2013-04-19 01:25:04 +0300

    clearing supernal ns

--- test/celestial/test/supernal.clj
+++
@@ -1,7 +0,0 @@
-(ns celestial.test.supernal
-   (:use 
-     midje.sweet
-     [supernal.core :only (apply-remote)]))
-
-(fact "neted remotes application"
-      (apply-remote '(let [1 2] (copy 1 2))) => '(let [1 2] ((copy 1 2) remote)))
