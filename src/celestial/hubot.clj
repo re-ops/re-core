fc2502abf2816fdaaefe26b2747793d1c4c6dcd8
narkisr <narkisr@gmail.com>
2013-05-13 18:27:31 +0300

    hubot post hook created

---
+++ src/celestial/hubot.clj
@@ -0,0 +1,16 @@
+(ns celestial.hubot
+  "Hubot notification post hook"
+  (:use 
+    [clojure.core.strint :only (<<)])
+  (:require 
+    [clj-http.client :as client]))
+
+
+(defn notify-hubot 
+  "notify a hubot instance that a machine is up" 
+  [{:keys [id hubot-host]}]
+  (client/post (<< "~{hubot-host}/hubot/creation-notify") 
+       {:body (<< "{\"id\": \"~{id}\"}") :content-type :json
+         :socket-timeout 1000 :conn-timeout 1000 :accept :json})) 
+
+
