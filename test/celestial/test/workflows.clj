29cbd6bd420e98d288ae75ae82c792612fff8615
Ronen Narkis <narkisr@gmail.com>
2013-06-09 00:06:54 +0300

    post error hook works

--- test/celestial/test/workflows.clj
+++ test/celestial/test/workflows.clj
@@ -5,17 +5,26 @@
     [celestial.workflows :only (resolve- run-hooks)])  
   (:import clojure.lang.ExceptionInfo))
 
-(defn foo-hook [v] v)
+(defn post-hook [v] v)
+(defn error-hook [v] v)
 
 (def identity-hook
-  {:hooks {:post-create {'celestial.test.workflows/foo-hook {:foo 1}}}})
+  {:hooks 
+    {:post-create {'celestial.test.workflows/post-hook {:foo 1}}
+     :post-error {'celestial.test.workflows/error-hook {:foo 1}}
+     }})
 
 (let [machine {:machine {:hostname "foo" :ip_address "192.168.2.1"}} merged (merge machine {:foo 1})]
   (with-redefs [config identity-hook]
     (fact "post hook invoke"
       (run-hooks machine :post-create) => nil
       (provided 
-        (foo-hook merged) => merged :times 1))))
+        (post-hook merged) => merged :times 1))
+    (fact "post error hook invoke"
+      (run-hooks machine :post-error) => nil
+      (provided 
+        (error-hook merged) => merged :times 1))
+    ))
 
 (fact "missing fn resolution error"
   (resolve- "non.existing/fn") => (throws ExceptionInfo))
