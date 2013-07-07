b5e9b1c5658ff45a1b26963eaef0d2e75874d363
Ronen Narkis <narkisr@gmail.com>
2013-07-07 17:53:49 +0300

    add midje repl

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,10 @@
 (ns user
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str]
-            [clojure.pprint :refer (pprint)]
-            [clojure.repl :refer :all]
-            [clojure.tools.namespace.repl :refer (refresh refresh-all)]
-            [celestial.launch :as launch]))
+  (:use midje.repl)
+  (:require 
+     [clojure.java.io :as io]
+     [clojure.string :as str]
+     [clojure.pprint :refer (pprint)]
+     [clojure.repl :refer :all]
+     [clojure.tools.namespace.repl :refer (refresh refresh-all)]
+     [celestial.launch :as launch]))
+
