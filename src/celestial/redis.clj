e6db5709d609b48345330eed2e15cc1fdd37fbb9
Ronen Narkis <narkisr@gmail.com>
2013-07-08 09:41:18 +0300

    clearing all duplicated fns

--- src/celestial/redis.clj
+++ src/celestial/redis.clj
@@ -17,6 +17,7 @@
     [celestial.common :only (get! curr-time gen-uuid half-hour minute)]
     [clojure.core.strint :only (<<)]
     [slingshot.slingshot :only  [throw+]]
+    [taoensso.carmine.locks :only (with-lock)]
     [taoensso.timbre :only (debug trace info error warn)])
   (:require  
     [taoensso.nippy :as nippy]
@@ -37,74 +38,30 @@
          )))
 
 
-(defn lock-id [k] (<< "lock.~{k}"))
+(defn get- [k] (wcar (car/get k)))
 
-(defn- expire [lid expiry]
-  (wcar (car/expire lid expiry)))
-
-(defn acquire 
-  "Acquires lock, returns uuid if successful 
-   wait-time (mili) wait time for lock,
-   expiry (sec) how long the lock is valid 
-   see http://dr-josiah.blogspot.co.il/2012/01/creating-lock-with-redis.html "
-  [id & [{:keys [wait-time expiry] :or {wait-time 2000 expiry 2} :as opts} & _]]
-  {:pre [(or (nil? opts) (associative? opts))]}
-  (let [lid (lock-id id) uuid (gen-uuid)
-        wait (+ (curr-time) wait-time)]
-    (loop []
-      (if (> wait (curr-time))
-        (if (= 1 (wcar (car/setnx lid uuid)))
-          (do (expire lid expiry) uuid)
-          (do 
-            (when (< (wcar (car/ttl lid)) 0)
-              (expire lid expiry))
-            (recur))) 
-        false)))) 
-
-(defn get- [k]
-  (wcar (car/get k)))
-
-(defn release [id uuid] 
-  (let [lid (lock-id id)]
-    (wcar (car/watch lid)) 
-    (if (= (get- lid)  uuid)
-      (do (wcar (car/del lid)) true)
-      (do (wcar (car/unwatch)) false))))
-
-(defn with-lock 
-  "Try to to obtain lock for id and execute f, throw exception if fails"
-  [id f & [opts]]
-  (trace "attempting to aquire lock on" id opts)
-  (if-let [uuid (acquire id opts)]
-    (try
-      (trace "lock acquired" id)
-      (f) 
-      (finally 
-        (release id uuid)
-        (trace "lock released" id)
-        ))
-    (throw+ {:type ::lock-fail :id id} "Failed to obtain lock")))
+; TODO this should be enabled in upstream
+(def ^:private lkey (partial car/kname "carmine" "lock"))
 
 (defn clear-locks []
   (trace "clearing locks")
-  (doseq [k (wcar (car/keys (lock-id "*")))]
-    (wcar (car/del k))))
+  (when-let [lkeys (seq (wcar (car/keys (lkey "*"))))]
+    (wcar (apply car/del lkeys))))
 
 (defn atom-key [k] (str "atom" k))
 
 (defn apply-diff [map-key _new old]
   {:pre [(map? _new) (map? old)]}
-  (fn [] 
     (letfn [(sub [a b] (difference (into #{} a) (into #{} b)))]
       (doseq [[k v] (sub old _new)] (wcar (car/hdel map-key k)))
-      (doseq [[k v] (sub _new old)] (wcar (car/hset map-key k (car/preserve v))))
-      )))
+      (doseq [[k v] (sub _new old)]  (wcar (car/hset map-key k (car/preserve v))))
+      ))
 
 (defn sync-watch [map-key r]
   (add-watch r map-key
      (fn [_key _ref old _new] 
-        (with-lock  _key (apply-diff map-key _new old)
-                 {:expiry half-hour :wait-time minute}))) r)
+       (with-lock _key half-hour minute 
+         (apply-diff map-key _new old)) r)))
 
 (defn synched-map [k]
   "Lock backed atom map watcher that persists changes into redis takes the backing redis hash key."
--- test/celestial/integration/redis.clj
+++ test/celestial/integration/redis.clj
@@ -2,55 +2,17 @@
   "Redis test assume a redis sandbox on localhost, use https://github.com/narkisr/redis-sandbox"
   (:use midje.sweet
         [celestial.fixtures :only (is-type?)]
-        [celestial.redis :only (acquire release get- with-lock synched-map clear-all clear-locks)])
+        [celestial.redis :only (get- synched-map clear-all clear-locks)])
   (:import clojure.lang.ExceptionInfo))
 
 (with-state-changes [(before :facts (clear-all))]
-  (fact "basic locks in redis" :integration :redis 
-        (acquire 2) => truthy ; for 2 sec
-        (acquire 2 {:wait-time 200}) => falsey ; too early
-        (Thread/sleep 1000)
-        (acquire 2) => truthy)
-
-  (fact "releasing locks" :integration :redis 
-        (let [uuid (acquire 3)]
-          (acquire 3 {:wait-time 200}) => falsey ; too early
-          (release 3 uuid) => truthy
-          (acquire 3) => truthy)) 
-
-  (fact "trying to release already released locks" :integration :redis 
-        (let [uuid (acquire 4 {:wait-time 2000}) f1 (future (release 4 uuid))]
-          (Thread/sleep 200); letting future run 
-          (release 4 uuid) => falsey)) 
-
-  (fact "locking scope" :integration :redis 
-        (try 
-          (with-lock 5 #(throw (Exception.)))
-          (catch Exception e nil))
-        (get- 5) => falsey) 
-
-  (fact "locking failure" :integration :redis 
-        (acquire 6 {:expiry 3}) 
-        (with-lock 6 #() {:wait-time 10}) => (throws ExceptionInfo (is-type? :celestial.redis/lock-fail)))
-
-  (fact "using lock expiry" :integration :redis 
-        (future (with-lock 9 (fn [] (Thread/sleep 1000)) {:expiry 4}))
-        (Thread/sleep 200)
-        (with-lock 9 #() {:expiry 3 :wait-time 10}) =>
-           (throws ExceptionInfo (is-type? :celestial.redis/lock-fail)))
-
-  (fact "clearing locks" :integration :redis locks-clearence 
-        (acquire 7 {:expiry 3}) 
-        (clear-locks) 
-        (acquire 7 {:expiry 3}) => truthy) 
-
   (fact "synched" :integration :redis 
-        (let [ids (synched-map :ids)]
-          (deref ids) => {} 
-          (swap! ids assoc :foo 1)    
-          (swap! ids assoc :bar 2)    
-          (deref (synched-map :ids)) => {:bar 2 :foo 1}
-          (reset! ids {:foo {:bar 2}})
-          (deref (synched-map :ids)) => {:foo {:bar 2}}
-          (reset! ids nil) => (throws java.lang.AssertionError)
-          (deref (synched-map :ids)) => {:foo {:bar 2}}))) 
+    (let [ids (synched-map :ids)]
+       (deref ids) => {} 
+       (swap! ids assoc :foo 1)    
+       (swap! ids assoc :bar 2)    
+       (deref (synched-map :ids)) => {:bar 2 :foo 1}
+       (reset! ids {:foo {:bar 2}})
+       (deref (synched-map :ids)) => {:foo {:bar 2}}
+       (reset! ids nil) => (throws java.lang.AssertionError)
+       (deref (synched-map :ids)) => {:foo {:bar 2}}))) 
