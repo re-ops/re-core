8066378807c011c0ff1dbdc0b7b3d173e506ea23
Ronen Narkis <narkisr@gmail.com>
2013-07-14 23:22:03 +0300

    copyright

--- src/aws/validations.clj
+++ src/aws/validations.clj
@@ -1,3 +1,14 @@
+(comment 
+   Celestial, Copyright 2012 Ronen Narkis, narkisr.com
+   Licensed under the Apache License,
+   Version 2.0  (the "License") you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.)
+
 (ns aws.validations
   "AWS based validations"
   (:use 
@@ -32,7 +43,6 @@
   :image-id [v/required cv/str?]
   :key-name [v/required cv/str?]
   )
-
 
 (defn provider-validation [{:keys [aws] :as spec}]
   (cv/validate!! ::invalid-aws aws aws-provider))
