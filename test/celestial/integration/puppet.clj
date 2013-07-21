705f7ebff31d5a5d6ce290efa0a4a023c1bff765
Ronen Narkis <narkisr@gmail.com>
2013-07-21 08:27:44 +0300

    vcenter setup testing

--- test/celestial/integration/puppet.clj
+++ test/celestial/integration/puppet.clj
@@ -11,7 +11,7 @@
     [celestial.workflows :only (reload puppetize destroy)]
     [celestial.config :only (path)]
     [celestial.redis :only (clear-all)]
-    [celestial.fixtures :only (with-conf redis-prox-spec redis-ec2-spec redis-type host puppet-ami)]))
+    [celestial.fixtures :only (with-conf redis-prox-spec redis-ec2-spec redis-vc-spec redis-type host puppet-ami)]))
 
 (defn run-cycle [spec type]
   (clear-all) 
@@ -23,11 +23,13 @@
       (finally 
         (destroy (assoc (p/get-system id) :system-id id))))))
 
-
-
 (fact "provisioning a proxmox instance" :integration :puppet :proxmox
       (with-conf
         (run-cycle redis-prox-spec redis-type)))
+
+(fact "provisioning a vcenter instance" :integration :puppet :vcenter
+      (with-conf
+        (run-cycle redis-vc-spec redis-type)))
 
 (fact "provisioning an ec2 instance" :integration :puppet :ec2
       "assumes a working ec2 defs in ~/.celestial.edn"
