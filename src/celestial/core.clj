64a7cf6f1cffeb56447f3d88dc8eece2382405c2
Ronen Narkis <narkisr@gmail.com>
2013-07-22 05:19:37 +0300

    bit more docs

--- src/celestial/core.clj
+++ src/celestial/core.clj
@@ -13,10 +13,10 @@
 
 (defprotocol Vm
   "A VM/Machine base API, implement this in order to add a new provider into celestial"
-  (create [this] "Creates a VM, the VM should be upand and ready to accept ssh sessions post this step")  
+  (create [this] "Creates a VM, the VM should be up and and ready to accept ssh sessions post this step")  
   (delete [this] "Deletes a VM")  
-  (start [this] "Starts an existing VM, ssh should be up")  
-  (stop [this]  "Stops a VM")
+  (start [this] "Starts an existing VM only if its not running, ssh should be up")  
+  (stop [this]  "Stops a VM only if it exists and running")
   (status [this] 
     "Returns vm status (values defere between providers) false if it does not exists"))
 
