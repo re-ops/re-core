6ee9695e07823ce51d0c52d5221910f87088507a
narkisr <narkisr@gmail.com>
2013-08-01 17:29:01 +0300

    range check

--- web/celestial/models/system.clj
+++ web/celestial/models/system.clj
@@ -1,14 +1,21 @@
 (ns celestial.models.system
   "System model for the frontend"
-  (:require [celestial.persistency :as p]))
+  (:require 
+    [slingshot.slingshot :refer [throw+]]
+    [clojure.core.strint :refer  (<<)]
+    [celestial.persistency :as p]))
 
 (defn systems 
-   "system list range" 
-   [from to]
-   (map (juxt identity p/get-system ) (subvec (into [] (p/all-systems)) from to)))
+  "system list range" 
+  [from to]
+  {:pre [(> from -1)]}
+  (let [systems (into [] (p/all-systems))]
+    (if (and (contains? systems from) (contains? systems to))
+      (map (juxt identity p/get-system) (subvec systems from to)) 
+      (throw+ {:type ::non-legal-range :message (<<  "No legal systems in range ~{from}:~{to} try between ~{0}:~(count systems)")}))))
 
 (defn systems-count 
   "How many systems there are"
-   []
+  []
   (count (p/all-systems)))
 
