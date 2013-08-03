6a9d61705f92b689b5a4468e5cead5595de79864
Ronen Narkis <narkisr@gmail.com>
2013-08-04 01:11:37 +0300

    bypassing https://github.com/yogthos/Selmer/issues/5

--- web/celestial/views/layout.clj
+++ web/celestial/views/layout.clj
@@ -1,10 +1,17 @@
 (ns celestial.views.layout
   (:use noir.request)
-  (:require [selmer.parser :as parser]))
+  (:require 
+    [selmer.template-parser :refer [preprocess-template]]
+    [selmer.util :refer (resource-path)] 
+    [selmer.parser :refer (parse render-template) ]))
 
 (def template-path "celestial/views/templates/")
 
+
 (defn render [template & [params]]
-  (parser/render-file (str template-path template)
-                      (assoc params :context (:context *request*))))
+  #_(parser/render-file (str template-path template)
+    (assoc params :context (:context *request*)))
+  (render-template 
+    (parse (java.io.StringReader. (preprocess-template (str template-path template))) {})
+       (assoc params :context (:context *request*))))
 
