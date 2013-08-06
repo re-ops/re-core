9f1ec414b6117e53723ff8a277f9cbd48a68e743
Ronen Narkis <narkisr@gmail.com>
2013-08-06 21:23:36 +0300

    testing bridged system and run seq

--- test/celestial/integration/proxmox.clj
+++ test/celestial/integration/proxmox.clj
@@ -1,10 +1,11 @@
 (ns celestial.integration.proxmox
   "Integration tests assume a proxmox vm with local address make sure to configure it"
-  (:use midje.sweet proxmox.provider
-        [flatland.useful.map :only (dissoc-in*)]
-        [celestial.common :only (slurp-edn)]
-        [celestial.model :only (vconstruct)]
-        [celestial.fixtures :only (with-conf redis-prox-spec)]))
+  (:require 
+     [flatland.useful.map :refer (dissoc-in*)]
+     [celestial.common :refer (slurp-edn)]
+     [celestial.fixtures :refer (with-conf redis-prox-spec redis-bridged-prox-spec)]  
+     [celestial.model :refer (vconstruct)])
+  (:use midje.sweet proxmox.provider))
 
 
 (def fake-id (update-in redis-prox-spec [:proxmox :vmid] (fn [o] 190)))
@@ -17,24 +18,24 @@
 
 ; generators 
 
-(fact "Full proxmox cycle" :integration :proxmox 
-  (with-conf
-    (let [ct (vconstruct redis-prox-spec)]
-     (.stop ct)
-     (.delete ct) 
-     (.create ct) 
-     (.start ct)
-     (.status ct) => "running"
-     (.stop ct)
-     (.delete ct))))
+(defn running-seq [ct]
+  (.stop ct)
+  (.delete ct) 
+  (.create ct) 
+  (.start ct)
+  (.status ct) => "running"
+  (.stop ct)
+  (.delete ct))
 
-(fact "ip and vmid generation" :integration :proxmox
+(fact "non generated" :integration :proxmox 
+  (with-conf
+    (running-seq (vconstruct redis-prox-spec))))
+
+(fact "ip and vmid generated" :integration :proxmox
    (with-conf
-    (let [ct (vconstruct (-> redis-prox-spec (dissoc-in* [:machine :ip]) (dissoc-in* [:proxmox :vmid])))]
-     (.stop ct)
-     (.delete ct) 
-     (.create ct) 
-     (.start ct)
-     (.status ct) => "running"
-     (.stop ct)
-     (.delete ct))))
+    (running-seq 
+      (vconstruct (-> redis-prox-spec (dissoc-in* [:machine :ip]) (dissoc-in* [:proxmox :vmid]))))))
+
+(fact "bridged" :integration :proxmox
+   (with-conf
+      (running-seq (vconstruct redis-bridged-prox-spec))))
