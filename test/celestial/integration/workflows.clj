b16ebbfe448d29ae10f1b6e5470191b39a0c3bce
Ronen Narkis <narkisr@gmail.com>
2013-10-19 23:05:50 +0300

    testing puppet workflow

--- test/celestial/integration/workflows.clj
+++ test/celestial/integration/workflows.clj
@@ -13,20 +13,49 @@
 (with-defaults 
   (with-state-changes [(before :facts (populate-system redis-type redis-prox-spec))]
     (fact "proxmox creation workflows" :integration :proxmox :workflow
-      (let [spec (assoc (s/get-system 1) :system-id 1)] 
-        (wf/create spec) => nil 
-        (wf/create spec) => (throws ExceptionInfo  (is-type? :celestial.workflows/machine-exists)) 
-        (wf/stop spec) => nil 
-        (wf/create spec) => (throws ExceptionInfo  (is-type? :celestial.workflows/machine-exists)) 
-        (wf/destroy spec) => nil))))
+      (letfn [(spec [] (assoc (s/get-system 1) :system-id 1))] 
+        (wf/create (spec)) => nil 
+        (wf/create (spec)) => (throws ExceptionInfo  (is-type? :celestial.workflows/machine-exists)) 
+        (wf/stop (spec)) => nil 
+        (wf/create (spec)) => (throws ExceptionInfo  (is-type? :celestial.workflows/machine-exists)) 
+        (wf/destroy (spec)) => nil))
+
+    (fact "proxmox reload workflows" :integration :ec2 :workflow
+      (letfn [(spec [] (assoc (s/get-system 1) :system-id 1))] 
+        (wf/create (spec)) => nil
+        (wf/reload (spec)) => nil 
+        (wf/destroy (spec)) => nil
+        ))
+    ))
 
 (with-admin
  (with-conf local-conf
-  (with-state-changes [(before :facts (populate-system redis-type redis-ec2-spec))]
+  (with-state-changes [(before :facts (do (populate-system redis-type redis-ec2-spec)))]
     (fact "aws creation workflows" :integration :ec2 :workflow
-      (let [spec (assoc (s/get-system 1) :system-id 1)] 
-        (wf/create spec) => nil 
-        (wf/create spec) => (throws ExceptionInfo  (is-type? :celestial.workflows/machine-exists)) 
-        (wf/stop spec) => nil 
-        (wf/create spec) => (throws ExceptionInfo  (is-type? :celestial.workflows/machine-exists)) 
-        (wf/destroy spec) => nil)))))
+      (letfn [(spec [] (assoc (s/get-system 1) :system-id 1))] 
+        (wf/create (spec)) => nil 
+        (wf/create (spec)) => (throws ExceptionInfo  (is-type? :celestial.workflows/machine-exists)) 
+        (wf/stop (spec)) => nil 
+        (wf/create (spec)) => (throws ExceptionInfo  (is-type? :celestial.workflows/machine-exists)) 
+        (wf/destroy (spec)) => nil
+        ))
+
+    (fact "aws reload workflows" :integration :ec2 :workflow
+      (letfn [(spec [] (assoc (s/get-system 1) :system-id 1))] 
+        (wf/create (spec)) => nil
+        (wf/reload (spec)) => nil 
+        (wf/destroy (spec)) => nil
+        ))
+
+    (fact "aws provision workflows" :integration :ec2 :workflow
+      (letfn [(spec [] (assoc (s/get-system 1) :system-id 1))] 
+        (wf/create (spec)) => nil
+        (wf/stop (spec)) => nil 
+        (wf/puppetize redis-type (spec)) => (throws java.lang.AssertionError)
+        (wf/start (spec)) => nil 
+        (wf/puppetize redis-type (spec)) => nil
+        (wf/destroy (spec)) => nil
+        ))
+    )))
+
+
