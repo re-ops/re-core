e1676a0ea7225c66feda5f5e6b5a24170e11ca56
Ronen Narkis <narkisr@gmail.com>
2013-11-03 18:27:44 +0200

    adding su user

--- src/celestial/roles.clj
+++ src/celestial/roles.clj
@@ -1,16 +1,41 @@
+(comment 
+  Celestial, Copyright 2012 Ronen Narkis, narkisr.com
+  Licensed under the Apache License,
+  Version 2.0  (the "License") you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.)
+
 (ns celestial.roles
-  "celetial roles")
+  "Celetial roles 
+  * Admin can access any machine and change any user
+  * Super user can access any machine but cant change users
+  * User can access only his machines (no user manipulation at all)
+  * Anonymous can't access any machine nor any other service
+  Note that by 'any machine' we mean any machine in the environments he is allowed 
+  to access.
+  ")
 
 (def ^{:doc "roles string to keyword map"}
-  roles-m {"admin" ::admin "user" ::user "anonymous" ::anonymous})
+  roles-m {"admin" ::admin "user" ::user "super-user" ::super-user "anonymous" ::anonymous})
 
 (def roles (into #{} (vals roles-m)))
 
 (derive ::admin ::user)
 
+(derive ::super-user ::user)
+
 (def user #{::user})
 
 (def admin #{::admin})
  
+(def su #{::super-user})
+
 (defn admin? [{:keys [roles] :as user}]
   (clojure.set/subset? admin roles))
+
+(defn su? [{:keys [roles] :as user}]
+  (clojure.set/subset? su roles))
