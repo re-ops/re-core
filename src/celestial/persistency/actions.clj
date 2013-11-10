4418d48abccfa264f638d41701037fddc04bb309
narkisr <narkisr@gmail.com>
2013-11-10 19:12:07 +0200

    simpler strucuture validation

--- src/celestial/persistency/actions.clj
+++ src/celestial/persistency/actions.clj
@@ -1,25 +1,27 @@
 (ns celestial.persistency.actions
   "Actions persistency"
   (:require
+    [slingshot.slingshot :refer  [throw+]]
     [subs.core :as subs :refer (validate!)]
+    [clojure.core.strint :refer (<<)]
     [puny.core :refer (entity)]))
 
 (entity action :indices [operates-on])
 
-(defn find-action-for [action-key type]
+(defn find-action-for [name type]
   (let [ids (get-action-index :operates-on type) 
         actions (map #(-> % Long/parseLong  get-action) ids)]
-    (first (filter #(-> % :actions action-key nil? not) actions))))
+    (first (filter #(= (-> % :name) name) actions))))
 
 (defn cap? [m] (contains? m :capistrano))
 
-(def cap-nested {:capistrano {:args #{:required :Vector}}})
+(def cap {:args #{:required :Vector}})
 
 (def action-base-validation
-  {:src #{:required :String} :operates-on #{:required :String :type-exists}})
+  {:src #{:required :String} :operates-on #{:required :String :type-exists}
+   :name #{:required :String}})
 
-(defn validate-action [{:keys [actions] :as action}]
-  (doseq [[k {:keys [capistrano] :as m}] actions] 
-    (when capistrano (validate! m cap-nested :error ::invalid-action)))
-  (validate! action action-base-validation :error ::invalid-nested-action ))
+(defn validate-action [{:keys [capistrano] :as action}]
+  (when capistrano (validate! capistrano cap :error ::invalid-cap-action))
+  (validate! action action-base-validation :error ::invalid-action ))
  
