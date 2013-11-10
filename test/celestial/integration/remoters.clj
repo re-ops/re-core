d4a7a57530dc7b29722b6e6a4ce28cc3dbba40fa
narkisr <narkisr@gmail.com>
2013-11-10 19:17:19 +0200

    populating users and assigning system-id to system

--- test/celestial/integration/remoters.clj
+++ test/celestial/integration/remoters.clj
@@ -3,38 +3,40 @@
   (:require 
     [celestial.persistency :as p] 
     [celestial.persistency.systems :as s]
-    [celestial.fixtures.data :refer (redis-actions redis-prox-spec redis-type)]
+    [celestial.fixtures.data :refer (redis-deploy redis-runall redis-prox-spec redis-type)]
+    [celestial.fixtures.populate :refer (add-users)]
     [celestial.fixtures.core :refer (with-defaults)]
     [celestial.redis :refer (clear-all)]
     [me.raynes.fs :refer (exists?)]
     [celestial.workflows :refer (reload destroy)]
-    [celestial.model :refer (rconstruct)]   
-     remote.capistrano)
-  (:use midje.sweet)
-  )
+    [celestial.model :refer (rconstruct)]
+    remote.capistrano)
+  (:use midje.sweet))
 
 (with-defaults
-  (with-state-changes [(before :facts (do (clear-all) (p/add-type redis-type)) )] 
+  (with-state-changes [(before :facts (do (clear-all) (p/add-type redis-type) (add-users)))] 
     (fact "basic deploy" :integration :capistrano
-      (let [id (s/add-system redis-prox-spec)
-            cap (rconstruct redis-actions {:action :deploy :target "192.168.5.200"})]
-         (reload redis-prox-spec)
+      (let [id (s/add-system redis-prox-spec) 
+            spec (assoc redis-prox-spec :system-id id)
+            cap (rconstruct redis-deploy {:target "192.168.5.200"})]
+         (reload spec)
          (.setup cap)
          (exists? (:dst cap)) => truthy 
          (.run cap)
          (.cleanup cap)
-         (destroy (assoc redis-prox-spec :system-id id)) 
+         (destroy spec) 
          (exists? (:dst cap))  => falsey 
          (s/system-exists? id)  => falsey)))
   
-    (fact "ruby run-all" :integration :ruby
+    (fact "ruby runall" :integration :ruby :capistrano
       (let [id (s/add-system redis-prox-spec)
-            cap (rconstruct redis-actions {:action :run-all :target "192.168.5.200"})]
-         (reload redis-prox-spec)
+            spec (assoc redis-prox-spec :system-id id)
+            cap (rconstruct redis-runall {:target "192.168.5.200"}) ]
+         (reload spec)
          (.setup cap)
          (exists? (:dst cap)) => truthy 
          (.run cap)
          (.cleanup cap)
-         (destroy (assoc redis-prox-spec :system-id id)) 
+         (destroy spec)
          (exists? (:dst cap))  => falsey 
          (s/system-exists? id)  => falsey))) 
