240e5750ab127e06699c4dda29c6c94b190c2f69
narkisr <narkisr@gmail.com>
2013-12-26 18:04:51 +0200

    matching results to new subnet

--- test/celestial/integration/networking.clj
+++ test/celestial/integration/networking.clj
@@ -8,18 +8,18 @@
 (with-conf
   (with-state-changes [(before :facts (do (clear-range :proxmox) (n/initialize-networking)))]
     (fact "used up ips marking" :integration :redis :ip
-      (n/gen-ip {} :proxmox :ip_address)  => {:ip_address "192.168.5.100"}
-      (count (n/list-free-ips :proxmox)) => 152
-      (n/list-used-ips :proxmox) => ["192.168.5.100" "192.168.5.170" "192.168.5.171" "192.168.5.173"]
-      (n/release-ip "192.168.5.100" :proxmox)
-      (n/list-used-ips :proxmox) => ["192.168.5.170" "192.168.5.171" "192.168.5.173"]
-      (count (n/list-free-ips :proxmox)) => 153)
+      (n/gen-ip {} :proxmox :ip_address)  => {:ip_address "192.168.3.200"}
+      (count (n/list-free-ips :proxmox)) => 52
+      (n/list-used-ips :proxmox) => ["192.168.3.200" "192.168.3.230" "192.168.3.231" "192.168.3.232"]
+      (n/release-ip "192.168.3.200" :proxmox)
+      (n/list-used-ips :proxmox) => ["192.168.3.230" "192.168.3.231" "192.168.3.232"]
+      (count (n/list-free-ips :proxmox)) => 53)
 
     (fact "multiple ips" :integration :redis :ip
       (dotimes [_ 5] (n/gen-ip {} :proxmox :ip_address))
-      (count (n/list-free-ips :proxmox)) => 148
-      (dotimes [i 5] (n/release-ip (str  "192.168.5.10" i) :proxmox)) 
-      (count (n/list-free-ips :proxmox)) => 153)
+      (count (n/list-free-ips :proxmox)) => 48
+      (dotimes [i 5] (n/release-ip (str  "192.168.3.20" i) :proxmox)) 
+      (count (n/list-free-ips :proxmox)) => 53)
     ))
 
 
