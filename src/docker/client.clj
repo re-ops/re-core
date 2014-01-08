b87ddfdd7abdb97b87e890df4a3d1498cea0ab4a
narkisr <narkisr@gmail.com>
2014-01-08 20:54:02 +0200

    implemented start/stop  listing and finding

--- src/docker/client.clj
+++ src/docker/client.clj
@@ -1,9 +1,10 @@
 (ns docker.client
   "Docker client"
   (:require 
+    [clojure.core.strint :refer (<<)]
+    [flatland.useful.seq :refer [find-first]]
     [clojure.string :as s]
-    [docker.remote :as r])
- )
+    [docker.remote :as r]))
 
 (defn camel-to-dash
   [s]
@@ -11,13 +12,11 @@
     (map #(if (Character/isUpperCase %) (str "-" (s/lower-case %)) %) s)))
 
 (def defaults
-  {:hostname "" :user "" :memory-swap 0
-    :attach-stdin false :attach-stdout true
-    :attach-stderr true :port-specs nil :tty false
-    :open-stdin false :stdin-once false :env nil
-    :cmd ["/bin/bash"] :dns nil 
-    :volumes { "/tmp"  {} } :volumes-from ""
-    :working-dir "" :exposed-ports { "22/tcp" {}}})
+  {:hostname "" :user "" :attach-stdin false :attach-stdout true
+   :attach-stderr true :port-specs nil :tty false
+   :open-stdin false :stdin-once false :env nil
+   :dns nil :volumes {} :volumes-from ""
+   :working-dir "" :exposed-ports { "22/tcp" {}}})
 
 (defn create 
    "Create a new container instance" 
@@ -25,14 +24,31 @@
   {:pre [(every? (partial contains? spec) [:image :memory] )]}
   (r/docker-post node "containers/create" (merge defaults spec)))
 
+(defn start 
+   "Start a new container instance" 
+   [node id]
+  (r/docker-post node (<< "containers/~{id}/start") {}))
+
+(defn stop
+   "Start a new container instance" 
+  ([node id] (stop node id 5))
+  ([node id timeout] (r/docker-post node (<< "containers/~{id}/stop?t~{timeout}") {})))
+
+(defn find-image
+   "find image id from tags, a tag has the form ubuntu:precise" 
+   [node tag]
+  (find-first (fn [{:keys [repo-tags]}] (find-first #{tag} repo-tags))
+    (r/docker-get node "images/json?all=0")))
+
+(defn list-containers 
+   "list all containers" 
+   [node]
+   (r/docker-get node "containers/json?all=0"))
 
 
 (comment 
-  (celestial.model/set-env :dev 
-  (try 
-    (create :local {:image "8dbd9e392a96" :memory 0})
-   (catch Throwable e
-     (clojure.pprint/pprint e)  
-     ) 
-    ))) 
-  
+ (-> (list-containers :local) first :id)
+ (create :local {:image "b36f06432104" :memory 0 :args ["-d" "-n"]})
+ (stop :local "032f7071997a286f1caadce4f8915cf9982df7c42d9c9a36eb1209250d340242")
+ (find-image :local "narkisr/sshd:latest")) 
+
