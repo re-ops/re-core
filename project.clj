024e2ff670b39c9fd5f6b8caf4e50f05c11db2b0
narkisr <narkisr@gmail.com>
2014-03-10 16:37:04 +0200

    latest subs with nested validations merging fix

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@
                  [amazonica "0.1.29"]
                  [narkisr/trammel "0.8.0-freez"]
                  [org.flatland/useful "0.10.3"]
-                 [substantiation "0.1.0"]
+                 [substantiation "0.1.1"]
                  [fogus/minderbinder "0.2.0"]
                  [ch.qos.logback/logback-classic "1.0.13"]
                  [org.codehaus.groovy/groovy "2.1.6"]
