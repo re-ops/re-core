d9b42b7a41e6aecc1b909c16b8961aa1fedb9965
narkisr <narkisr@gmail.com>
2014-03-13 02:36:30 +0200

    latest self build

--- project.clj
+++ project.clj
@@ -47,7 +47,7 @@
   :exclusions [org.clojure/clojure]
 
   :plugins  [[jonase/eastwood "0.1.0"] 
-             [self-build "0.0.1"]
+             [self-build "0.0.4"]
              [lein-ancient "0.4.2"] [lein-tar "2.0.0" ]
              [lein-tag "0.1.0"] [lein-set-version "0.3.0"]
              [topping "0.0.2"] [self-build "0.0.3"]]
