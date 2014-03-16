5d3cf2c50d9ecb8f0140e2522287b530c347c382
narkisr <narkisr@gmail.com>
2014-03-16 20:35:52 +0200

    basic searching works

--- test/celestial/integration/es/systems.clj
+++ test/celestial/integration/es/systems.clj
@@ -2,10 +2,20 @@
   "Testing system searching"
   (:require 
     [es.node :as es]
-    [es.systems :as esys])
+    [es.systems :as sys]
+    [clojurewerkz.elastisch.query :as q]
+    [celestial.fixtures.data :refer (redis-prox-spec redis-ec2-spec)]
+    )
   (:use midje.sweet))
 
-(with-state-changes [(before :facts (do (es/start-n-connect) (esys/initialize)))
+(with-state-changes [(before :facts (do (es/start-n-connect) (sys/initialize)))
                      (after :facts (es/stop))]
-  
-  )
+   
+  (fact "basic system put and get" :integration :elasticsearch
+    (sys/put "1" redis-prox-spec)        
+    (sys/put "2" redis-ec2-spec)        
+    (sys/put "3" redis-prox-spec)        
+    (get-in (sys/get "1") [:source :env]) => ":dev"
+    (get-in (sys/query {:bool {:must {:term {"machine.cpus" "4" }}}}) [:hits :total]) => 2
+        
+    ))
