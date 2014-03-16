6badfffddfad4b53f76dc00e377412a2eefa5fd2
narkisr <narkisr@gmail.com>
2014-03-16 16:09:15 +0200

    external settings and cleaner API

--- src/es/node.clj
+++ src/es/node.clj
@@ -1,35 +1,66 @@
+(comment 
+   Celestial, Copyright 2012 Ronen Narkis, narkisr.com
+   Licensed under the Apache License,
+   Version 2.0  (the "License") you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.)
+
 (ns es.node
   "An embedded ES node instance"
+  (:import 
+    [org.elasticsearch.node NodeBuilder])
   (:require 
+    [celestial.common :refer (get!)]
+    [clojurewerkz.elastisch.native.conversion :as cnv]
     [clojurewerkz.elastisch.native :as es]
-    [clojurewerkz.elastisch.native.index :as idx]
-    [clojurewerkz.elastisch.native.document :as doc]))
+    ))
 
 (def ES (atom nil))
 
-(defn start-node 
+(defn build-local-node
+  [settings]
+    (.build 
+      (doto (NodeBuilder/nodeBuilder)
+      (.settings (cnv/->settings settings)) 
+      (.data true)
+      (.local true))))
+
+
+(defn settings 
+   "embedded ES settings" 
+   []
+   (merge (get! :elasticsearch) {
+      :node.name "celestial" :cluser.name "celestial-cluster"
+      :local true :http.enabled false 
+    }))
+
+(defn start
   "launch en embedded ES node" 
   []
-  (reset! ES 
-          (es/start-local-node
-            (es/build-local-node
-              {:node.name "celestial" 
-               :cluser.name "celestial-cluster"
-               :path.logs "/tmp/celestial-es"
-               :path.data "/tmp/celestial-es"
-               :path.work "/tmp/celestial-es"
-               :local true :http.enabled false }))))
+  (reset! ES (es/start-local-node (build-local-node (settings)))))
 
-(defn stop-node 
+(defn connect 
+   []
+  (es/connect-to-local-node! @ES))
+
+(defn start-n-connect 
+   "Both starts the node and connects to it" 
+   []
+  (start)
+  (connect))
+
+(defn stop
   "stops embedded ES node" 
   []
   (.close @ES) 
   (reset! ES nil))
 
-(comment (start-node)
-         (stop-node)
-         ;; (println @ES)
-         (es/connect-to-local-node! @ES)
-         (idx/create "myapp2_development" )
-         (doc/put "myapp2_development" "person"  "10" {:username "ronen"})
-         (doc/get "myapp2_development" "person" "10"))
+;; (start)
+;; (stop)
+;; (idx/create "myapp2_development" )
+;; (doc/put "myapp2_development" "person"  "10" {:username "ronen"})
+;; (doc/get "myapp2_development" "person" "10")
