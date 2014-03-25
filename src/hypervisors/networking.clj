f6b5ec864d66b6694b8846bad70c32a4eba3bd05
narkisr <narkisr@gmail.com>
2014-03-25 14:11:10 +0200

    seperating scanning

--- src/hypervisors/networking.clj
+++ src/hypervisors/networking.clj
@@ -15,39 +15,14 @@
     [selmer.filters :refer (add-filter!)]
     [clojure.core.strint :refer [<<]]
     [selmer.parser :refer (render-file)]
-    [clojure.data :refer [diff]]
-    [clojure.java.data :refer [from-java]] 
     [celestial.common :refer [get! import-logging]]
     [celestial.model :refer [hypervisor get-env! set-env]]
     [slingshot.slingshot :refer [throw+ try+]]
     [celestial.redis :refer [wcar]]
     [flatland.useful.utils :refer (defm)]
-    [taoensso.carmine :as car])
-  (:import 
-    org.nmap4j.Nmap4j 
-    org.nmap4j.parser.OnePassParser))
+    [taoensso.carmine :as car]))
 
 (import-logging)
-
-(defn ports-open? [host]
-  (not (nil? (first (filter #(= % "open") (map #(get-in % [:state :state]) (get-in host [:ports :ports])))))))
-
-(defn raw-scan 
-  "runs raw scan, require nmap bin to be symlinked to /usr/local"
-  [ip]
-  (let [nmap (Nmap4j. "/usr/local")]
-    (doto nmap 
-      (.includeHosts ip)
-      (.addFlags "-n -T5 -p U:1194,T:21,22,25,53,80,110,111")
-      (.execute) 
-      (.getResult))))
-
-(defn hosts-scan 
-  "Uses nmap so scan a list of hosts (192.168.10.1-50)"
-  [hosts]
-  (map :addresses 
-       (filter ports-open?  (get-in (from-java (raw-scan hosts)) [:result :hosts]))))
-
 
 (defn ip-to-long 
   "Converts an ip address to long number" 
@@ -162,12 +137,6 @@
   (map #( -> % (Long/parseLong) long-to-ip) (wcar (car/zrangebyscore (ips-key hyp) 0 0))))
 
 ;; (celestial.model/set-env :dev (list-used-ips :proxmox)) 
-
-(defn correlate
-  "compares stored ips to scan result"
-  [k]
-  (let [scanned (map (fn [[{:keys [addr]}]] addr) (hosts-scan "192.168.20.170-254"))]
-    (zipmap [:scanned :listed :common] (diff (into #{} scanned) (into #{} (list-used-ips k))))))
 
 (defn clear-range 
   "mainly for testing" 
