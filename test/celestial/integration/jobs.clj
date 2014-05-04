b7b1832fc592d994c733c2ef26d5580d8c63d174
narkisr <narkisr@gmail.com>
2014-05-05 01:30:56 +0300

    changing test to match jobs fn

--- test/celestial/integration/jobs.clj
+++ test/celestial/integration/jobs.clj
@@ -1,11 +1,14 @@
 (ns celestial.integration.jobs
   (:require 
+    [celestial.model :refer (operations)]
     [celestial.fixtures.core :refer (with-conf)]  
-    [celestial.jobs :as jobs :refer (initialize-workers workers)])
+    [celestial.jobs :refer (jobs initialize-workers workers)])
   (:use midje.sweet))
 
 (with-conf 
-  (with-state-changes [(before :facts (reset! jobs/jobs {:machine [identity 2]}))] 
+  (with-state-changes [(before :facts (reset! workers {}))]
     (fact "workers creation" :integration :redis
-       (initialize-workers)
-       (keys @workers) => (just :machine))))
+      (initialize-workers) => nil
+      (provided 
+        (jobs) => {:machine [identity 2]})
+      (keys @workers) => (just :machine))))
