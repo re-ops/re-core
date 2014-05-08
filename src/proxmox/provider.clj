5da7185da9b4eec9dd278584556bc1e530141ff5
narkisr <narkisr@gmail.com>
2014-05-08 13:58:54 +0300

    rethrowing using throw+

--- src/proxmox/provider.clj
+++ src/proxmox/provider.clj
@@ -141,7 +141,7 @@
               (->Container node ct* extended network*)
               (catch [:status 500] e 
                 (warn "Container already exists" e) (throw e))
-              (catch Throwable e (release-ip ip :proxmox) (throw e)))))
+              (catch Throwable e (release-ip ip :proxmox)  (throw+ e)))))
 
   (delete [this]
      (try 
