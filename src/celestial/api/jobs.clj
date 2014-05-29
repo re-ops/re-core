c7d3b7d50b6a6e5776ae3aa4db1c448575527c71
narkisr <narkisr@gmail.com>
2014-05-29 19:01:42 +0300

    return empty map on nil

--- src/celestial/api/jobs.clj
+++ src/celestial/api/jobs.clj
@@ -132,4 +132,7 @@
   
   (GET- "/jobs/done/:tid" [^:string tid ]
       {:nickname "getDoneJobByTid" :summary "Get a done job using its tid"}
-     (success (es/get tid))))
+    (let [job (es/get tid)]
+     (if-not job 
+       (success {})
+       (success (:source job))))))
