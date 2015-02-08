d16b70224489812b48613044485877e38789cdcf
narkisr <narkisr@gmail.com>
2015-02-09 00:52:31 +0200

    supporting instace ephmeral storage

--- test/celestial/integration/workflows/ec2.clj
+++ test/celestial/integration/workflows/ec2.clj
@@ -40,8 +40,14 @@
            (wf/destroy (spec)) => nil
           ))
 
-      (fact "aws with volumes" :integration :ec2 :workflow
+      (fact "aws with ebs volumes" :integration :ec2 :workflow
         (let [with-vol {:aws {:volumes [{:device "/dev/sdn" :size 10 :clear true :volume-type "standard" }]}}]
+          (wf/create (spec with-vol)) => nil
+          (wf/reload (spec with-vol)) => nil 
+          (wf/destroy (spec with-vol)) => nil))
+
+      (fact "aws with ephemeral volumes" :integration :ec2 :workflow
+        (let [with-vol {:aws {:instance-type "m1.small" :block-devices [{:device-name "/dev/sdb" :virtual-name "ephemeral0" }]}}]
           (wf/create (spec with-vol)) => nil
           (wf/reload (spec with-vol)) => nil 
           (wf/destroy (spec with-vol)) => nil))
