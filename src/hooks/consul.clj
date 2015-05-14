8e165b5d05b4dabed666994cea8f63ffe2ee3483
ronen <narkisr@gmail.com>
2015-05-14 16:25:25 +0300

    consul by env

--- src/hooks/consul.clj
+++ src/hooks/consul.clj
@@ -20,20 +20,26 @@
 (defn add-node
   [{:keys [system-id consul]:as args}]
   (let [{:keys [machine env]} (s/get-system system-id)
-        {:keys [dc host]} (consul env)
-        res (register host (machine :hostname) (machine :ip) dc)]
-    (debug "registered node" res)))
+        {:keys [dc host] :as c} (consul env)]
+    (when c 
+      (register host (machine :hostname) (machine :ip) dc)
+      (debug "registered node in consul host" host "dc" dc))))
 
 (defn remove-node
   [{:keys [env machine consul]:as args}]
-  (let [{:keys [dc host]} (consul env)
-        res (de-register host (machine :hostname) dc)]
-    (debug "removed node" res)))
+  (when-let [{:keys [dc host]} (consul env)]
+    (de-register host (machine :hostname) dc)
+    (debug "removed node from consul host" host "dc" dc)))
 
 
 (def actions {:reload {:success add-node} :create {:success add-node} 
               :destroy {:success remove-node :error remove-node}
               :stage {:success add-node}})
 
+(defn with-defaults 
+  "Add an empty consul if not defined"
+  [args]
+  (merge {:consul {}} args))
+
 (defn update-node [{:keys [event workflow] :as args}]
-    (when-let [action (get-in actions [workflow event])] (action args)))
+    (when-let [action (get-in actions [workflow event])] (action (with-defaults args))))
