21ddbbac511424ddb73811e7bc96f00e9b9aedb3
ronen <narkisr@gmail.com>
2015-05-20 03:25:20 +0300

    provider skeleton

--- src/freenas/provider.clj
+++ src/freenas/provider.clj
@@ -1,4 +1,30 @@
 (ns freenas.provider
   "Freenas jails provider"
+  (:require 
+    [freenas.remote]
+    [slingshot.slingshot :refer  [throw+]]
+    [celestial.persistency.systems :as s]
+    [celestial.common :refer (import-logging)]
+    [celestial.provider :refer (wait-for wait-for-ssh)]
+    [celestial.core :refer (Vm)] 
+    [celestial.model :refer (hypervisor translate vconstruct)])
  )
 
+(import-logging)
+
+(defrecord Instance [spec]
+  Vm
+  (create [this] 
+    )
+
+  (start [this]
+     )
+
+  (delete [this]
+     )
+
+  (stop [this]
+     )
+
+  (status [this] 
+     ))
--- src/freenas/remote.clj
+++ src/freenas/remote.clj
@@ -1,22 +1,25 @@
 (ns freenas.remote
   (:require 
+    [celestial.common :refer (import-logging)]
     [cheshire.core :refer :all]
+    [celestial.model :refer (hypervisor)]
     [clojure.core.strint :refer (<<)]
     [org.httpkit.client :as client])
  )
 
+(defn freenas [ks]
+  )
 (defn root 
    []
-  "https://freenas/api/v1.0/"
-  )
+   (<< "https://~(hypervisor :freenas :host)/api/v1.0/"))
 
 (defn auth  
    []
-  ["root" ""]
+  [(hypervisor :freenas :user) (hypervisor :freenas :password)]
   )
 
 (defn call [verb api args]
- @(verb (<< "~(root)~{api}") (merge args {:basic-auth (auth) :insecure? true})))
+   @(verb (<< "~(root)~{api}") (merge args {:basic-auth (auth) :insecure? true})))
 
 
 ;; (clojure.pprint/pprint (parse-string (:body (call client/get  "jails/jails/?format=json" {})) true))
