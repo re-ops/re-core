f32c63edda9a5e50c025cac73e9a76547b713db4
ronen <narkisr@gmail.com>
2015-06-01 13:17:19 +0300

    fixing yaml generation

--- src/celestial/puppet_standalone.clj
+++ src/celestial/puppet_standalone.clj
@@ -47,7 +47,7 @@
     
    (task copy-yaml
      (let [{:keys [type module]} args path (<< "/tmp/~(type :hostname).yml") f (file path) ]
-       (spit f (yaml/generate-string (select-keys type [:classes])))
+       (spit f (yaml/generate-string (module :classes)))
        (debug "copy from " path remote)
        (copy path (<< "/tmp/~(module :name)/"))
        (.delete f))) 
