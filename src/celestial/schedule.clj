d405b30e8706f59bab5b2d0f7d2b79184ca1ac6b
ronen <narkisr@gmail.com>
2015-06-08 18:21:14 +0300

    closing channels

--- src/celestial/schedule.clj
+++ src/celestial/schedule.clj
@@ -6,17 +6,15 @@
    [components.core :refer (Lifecyle)]
    [clj-time.core :as t]
    [clj-time.periodic :refer  [periodic-seq]]
-   [clojure.core.async :as a :refer [<! <!! go-loop]]))  
+   [clojure.core.async :as a :refer [<! close! go-loop]]))  
 
 (import-logging)
 
-(defn schedule [f t args]
-  (let [chimes (chime-ch t)]
-    (<!! 
-      (go-loop []
-        (when-let [msg (<! chimes)]
-         (f args)
-         (recur))))))
+(defn schedule [f chimes args]
+ (go-loop []
+   (when-let [msg (<! chimes)]
+     (f args)
+     (recur))))
 
 (defn time-fn [unit]
   (resolve-  (symbol (str "clj-time.core/" (name unit)))))
@@ -26,15 +24,23 @@
    []
   (doseq [[f m] (get* :scheduled) :let [{:keys [every args]} m]]
     (let [[t unit] every]
-      (schedule (resolve- f) (periodic-seq (t/now) ((time-fn unit) t)) args))))
+      (schedule (resolve- f) (chime-ch (periodic-seq (t/now) ((time-fn unit) t))) args))))
+
+(defn schedules []
+  (map 
+    (fn [[f {:keys [every args]}]]
+      (let [[t unit] every]
+        [(resolve- f) (chime-ch (periodic-seq (t/now) ((time-fn unit) t))) args])) (get* :scheduled)))
 
 (defrecord Schedule
-  [] 
+  [scs] 
   Lifecyle
   (setup [this])
   (start [this]
     (info "Starting scheduled tasks")
-    (load-schedules))
-  (stop [this]))
+    (doseq [s scs] (apply schedule s)))
+  (stop [this]
+    (doseq [[_ c _] scs] (close! c))))
 
-(defn instance [] (Schedule.))
+(defn instance []
+  (Schedule. (schedules)))
