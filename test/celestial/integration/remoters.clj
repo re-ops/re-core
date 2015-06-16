21259e7c6665014c0277f29f106bd2630efd6328
ronen <narkisr@gmail.com>
2015-06-16 17:59:25 +0300

    not working yet but using more latest populate fn

--- test/celestial/integration/remoters.clj
+++ test/celestial/integration/remoters.clj
@@ -4,39 +4,35 @@
     [celestial.persistency.types :as t] 
     [celestial.persistency.systems :as s]
     [celestial.fixtures.data :refer (redis-deploy redis-runall redis-prox-spec redis-type)]
-    [celestial.fixtures.populate :refer (add-users)]
     [celestial.fixtures.core :refer (with-defaults)]
-    [celestial.redis :refer (clear-all)]
     [me.raynes.fs :refer (exists?)]
+    [celestial.integration.workflows.common :refer (spec get-spec)]
+    [celestial.fixtures.populate :refer (populate-system)]  
     [celestial.workflows :refer (reload destroy)]
     [celestial.model :refer (rconstruct)]
     remote.capistrano)
   (:use midje.sweet))
 
 (with-defaults
-  (with-state-changes [(before :facts (do (clear-all) (t/add-type redis-type) (add-users)))] 
+  (with-state-changes [(before :facts (populate-system redis-type redis-prox-spec))] 
     (fact "basic deploy" :integration :capistrano
-      (let [id (s/add-system redis-prox-spec) 
-            spec (assoc redis-prox-spec :system-id id)
-            cap (rconstruct redis-deploy {:target "192.168.3.200"})]
-         (reload spec)
+      (let [cap (rconstruct redis-deploy {:target "192.168.3.200"})]
+         (reload (spec))
          (.setup cap)
          (exists? (:dst cap)) => truthy 
          (.run cap)
          (.cleanup cap)
-         (destroy spec) 
+         (destroy (spec)) => nil
          (exists? (:dst cap))  => falsey 
-         (s/system-exists? id)  => falsey)))
+         )))
   
-    (fact "ruby runall" :integration :ruby :capistrano
-      (let [id (s/add-system redis-prox-spec)
-            spec (assoc redis-prox-spec :system-id id)
-            cap (rconstruct redis-runall {:target "192.168.3.200"}) ]
-         (reload spec)
+   (fact "ruby runall" :integration :ruby :capistrano
+      (let [cap (rconstruct redis-runall {:target "192.168.3.200"}) ]
+         (reload (spec))
          (.setup cap)
          (exists? (:dst cap)) => truthy 
          (.run cap)
          (.cleanup cap)
-         (destroy spec)
+         (destroy (spec)) => nil
          (exists? (:dst cap))  => falsey 
          (s/system-exists? id)  => falsey))) 
