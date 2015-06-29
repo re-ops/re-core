7391f10273039a74e9cf8968bdef9f59aa2341b7
ronen <narkisr@gmail.com>
2015-06-29 15:30:01 +0300

    basic persistency

--- test/celestial/persistency/stacks.clj
+++ test/celestial/persistency/stacks.clj
@@ -1,4 +1,4 @@
-(ns celestial.integration.persistency.stacks
+(ns celestial.persistency.stacks
   "Testing basic stacks persisetency"
   (:require 
     [celestial.persistency.stacks :as s]
@@ -10,7 +10,10 @@
 
 (with-conf
   (with-state-changes [(before :facts (re-initlize))]
-   (fact "basic stack usage" :integration :redis :stacks
-     (s/add-stack simple-stack) => nil
-     )
+    (fact "basic stack persistency" :persistency :redis :stacks
+      (s/add-stack simple-stack) => 1
+      (s/update-stack "1" (assoc-in simple-stack [:shared :env] :qa)) 
+      (get-in (s/get-stack 1) [:shared :env])  => :qa
+      (s/delete-stack "1") => 1
+      )
    ))
