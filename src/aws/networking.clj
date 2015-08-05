2584aaf9d050bdcd9a40125c84082aca20943675
ronen <narkisr@gmail.com>
2015-08-05 03:20:48 +0300

    removing allocation id

--- src/aws/networking.clj
+++ src/aws/networking.clj
@@ -81,7 +81,7 @@
       (with-ctx ec2/associate-address {:instance-id instance-id :allocation-id allocation-id})))
 
 (defn assoc-pub-ip [endpoint instance-id {:keys [machine aws] :as spec}]
-   (let [{:keys [ip]} machine {:keys [subnet-id allocation-id]} aws]
+   (let [{:keys [ip]} machine {:keys [subnet-id]} aws]
      (if-not subnet-id
        (with-ctx ec2/associate-address {:instance-id instance-id :public-ip ip})
        (attach-vpc-ip endpoint instance-id spec))))
