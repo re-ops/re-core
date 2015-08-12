510563d617e4845d4fe76e0db19bbeec6970cc12
ronen <narkisr@gmail.com>
2015-08-12 18:28:20 +0300

    supporting running specific classes

--- src/celestial/puppet_standalone.clj
+++ src/celestial/puppet_standalone.clj
@@ -82,8 +82,12 @@
       ))) 
 
 
-(defmethod pconstruct :puppet-std [{:keys [run-opts] :as type} {:keys [machine env] :as spec}]
-  (let [remote {:host (machine :ip) :user (or (machine :user) "root")}
-        by-env (get-in type [:puppet-std env])
-        with-opts (update-in by-env [:args] (fn [a] (concat a ((or run-opts {}) :args))))]
-    (Standalone. remote (assoc with-opts :hostname (machine :hostname)))))
+(defmethod pconstruct :puppet-std 
+  [{:keys [run-opts] :as type :or {run-opts {}}} {:keys [machine env] :as spec}]
+  (let [remote {:host (machine :ip) :user (or (machine :user) "root")}]
+    (Standalone. remote
+      (-> (get-in type [:puppet-std env])
+        (update-in [:args] (fn [a] (concat a (run-opts :args))))
+        (merge (select-keys run-opts [:classes]))
+        (assoc :hostname (machine :hostname))
+        ))))
