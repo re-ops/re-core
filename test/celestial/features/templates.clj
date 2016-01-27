1fd9cb127d0be762be66f8ca5ad6b5da4767adc3
narkisr <narkisr@gmail.com>
2016-01-27 16:09:44 +0200

    indenting

--- test/celestial/features/templates.clj
+++ test/celestial/features/templates.clj
@@ -11,7 +11,7 @@
   (with-conf
     (with-state-changes [(before :facts (populate-all :skip :templates))]
       (fact "basic template persistency" :integration :redis :templates
-      (let [id (s/add-template small-redis)]
-        (s/get-template id) => (contains {:type "redis" :name "small-redis"})) 
-      (let [provided {:env :dev :owner "admin" :machine {:hostname "foo" :domain "local"}}]
-        (s/templatize "small-redis" provided) => 101)))))
+        (let [id (s/add-template small-redis)]
+          (s/get-template id) => (contains {:type "redis" :name "small-redis"})) 
+        (let [provided {:env :dev :owner "admin" :machine {:hostname "foo" :domain "local"}}]
+          (s/templatize "small-redis" provided) => 101)))))
