ecaa94b6c87068b8fe2e7e2915a0dc859ac0a30e
ronen <narkisr@gmail.com>
2017-02-16 01:20:06 +1100

    rm impl

--- src/celestial/repl.clj
+++ src/celestial/repl.clj
@@ -1,6 +1,6 @@
 (ns celestial.repl
   "Repl Driven Celestial "
-  (:require 
+  (:require
     [celestial.persistency.systems :as s]
     [celestial.persistency.types :as t]
     [es.systems :as es :refer (set-flush)]
@@ -14,18 +14,18 @@
 
 (def admin {
   :username "admin" :password "foo"
-  :envs [:dev :qa :prod] 
-  :roles #{:celestial.roles/admin} 
+  :envs [:dev :qa :prod]
+  :roles #{:celestial.roles/admin}
   :operations [] })
 
 (defmacro with-admin [& body]
   `(with-redefs [celestial.security/current-user (fn [] {:username "admin"})
                 celestial.persistency.users/get-user! (fn [a#] admin)]
-       ~@body 
+       ~@body
        ))
 
-(defprotocol Repl 
-  "Basic shell like functions on Celestial model types" 
+(defprotocol Repl
+  "Basic shell like functions on Celestial model types"
   (ls [this] [this & opts])
   (find [this exp])
   (rm [this items])
@@ -39,8 +39,8 @@
     (= v (sub k))
     ))
 
-(defrecord Systems [] Repl 
-  (ls [this] 
+(defrecord Systems [] Repl
+  (ls [this]
     (with-admin
       (let [systems (into [] (s/systems-for (celestial.api.systems/working-username)))]
         [this {:systems (doall (map (juxt identity s/get-system) systems))}])))
@@ -49,26 +49,28 @@
   (find [this exp])
   (rm [this systems]
     (doseq [id (map first systems)]
-      (s/delete-system! (Integer/valueOf id))))
+      (s/delete-system! (Integer/valueOf id)))
+     [this {:systems []}]
+    )
   (grep [this systems k v]
       [this {:systems (filter (partial grep-system k v) (systems :systems))}]))
 
 
-(defrecord Types [] Repl 
+(defrecord Types [] Repl
   (ls [this]
     [this {:types (map t/get-type (t/all-types))}])
   (ls [this & opts])
   (find [this exp])
   (rm [this types ])
-  (grep [this types k v]) 
+  (grep [this types k v])
   )
 
 
-(defmulti pretty 
+(defmulti pretty
   (fn [[_ m]] (clojure.set/intersection (into #{} (keys m)) #{:systems :types})))
 
 (defn render [[id m]]
- (-> m 
+ (-> m
    (select-keys* [:owner] [:machine :hostname] [:machine :os] [:machine :ip])
    (assoc :id id)))
 
@@ -76,7 +78,7 @@
   (let [formatter (format-columns bold-white-font [:right 10] "  " reset-font [:right 2] "  "
                      [:right 5] "  " [:right 12] "  " :none)]
     (write-rows *out* formatter [:hostname :id :owner :os :ip] (map render systems))))
- 
+
 (defmethod pretty #{:types} [[_ {:keys [types]}]]
   (let [formatter (format-columns bold-white-font [:right 10] "  " reset-font [:right 10] "  "[:right 20] :none)]
     (write-rows *out* formatter [:type (comp first keys) :description] types)))
@@ -91,5 +93,4 @@
 (def types (Types.))
 
 ;; (pretty (ls types))
-;; (macroexpand-1 '(|> (ls systems) (grep :os :ubuntu-14.04 :four)))
-;; (pretty (|> (ls systems) (grep :os :ubuntu-14.04 )))
+;; (pretty (|> (ls systems) (grep :os :ubuntu-14.04)))
