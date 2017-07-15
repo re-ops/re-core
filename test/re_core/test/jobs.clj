a063689f94a444f81b7e846524e035b57da55719
ronen <narkisr@gmail.com>
2017-07-16 01:54:16 +1000

    fixing build

--- test/re_core/test/jobs.clj
+++ test/re_core/test/jobs.clj
@@ -1,25 +1,22 @@
 (ns re-core.test.jobs
-  (:require 
+  (:require
     [re-core.persistency.systems :as s]
     [taoensso.carmine :as car]
     [re-core.redis :refer (server-conn)]
     [taoensso.carmine.locks :refer (with-lock acquire-lock)]
     [re-core.common :refer (minute)]
     [clojure.core.strint :refer (<<)]
-    [re-core.jobs :refer (initialize-workers workers job-exec create-wks enqueue)] 
+    [re-core.jobs :refer (initialize-workers workers job-exec create-wks enqueue)]
     [re-core.jobs :as jobs])
   (:use midje.sweet)
   (:import java.lang.AssertionError))
 
 
-(fact "with-lock used if :identity key was provided" 
+(fact "with-lock used if :identity key was provided"
    (job-exec identity {:message {:identity "red1" :args {:machine {:hostname "red1"}}} :attempt 1 :user "ronen"}) => {:status :success}
-   (provided 
+   (provided
      (s/get-system "red1") => {:machine {:hostname "red1"}}
-     (server-conn) => {}
-     (acquire-lock {} "red1" 1800000 300000) => nil :times 1
-     (jobs/save-status anything :success)  => {:status :success}  :times 1
-     ))
+     (jobs/save-status anything :success)  => {:status :success}  :times 1))
 
 
 (fact "enqueue to workless queue should fail"
