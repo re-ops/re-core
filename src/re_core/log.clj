15a3eb9d834802f29448f197f140231a3d40c264
ronen <narkisr@gmail.com>
2017-08-18 02:03:29 +1000

    ignoring ns

--- src/re_core/log.clj
+++ src/re_core/log.clj
@@ -23,6 +23,8 @@
     (merge-config!
      {:appenders {:rolling (rolling-appender {:path (log* :path) :pattern :weekly})}})
     (merge-config!
+     {:ns-blacklist ["net.schmizz.*" "org.elasticsearch.indices.*"]})
+    (merge-config!
      {:appenders {:println {:ns-whitelist ["re-core"]}}})
     (disable-coloring)
     (set-level! (log* :level))))
