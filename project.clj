d5e7636a6e8be5ba048a7d330a7230dfbf251888
ronen <narkisr@gmail.com>
2018-01-08 02:03:41 +1100

    latest re-mote

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
                  [com.taoensso/tufte "1.1.1"]
 
                  ; re-ops
-                 [re-mote "0.7.0"]
+                 [re-mote "0.7.1"]
                  [re-share "0.3.0"]
 
                  ;api
