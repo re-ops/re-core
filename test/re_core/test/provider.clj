d261a861c41d01b50d736b53bd96812aa403d5d3
ronen <narkisr@gmail.com>
2018-03-20 00:30:21 +1100

    cleanup

--- test/re_core/test/provider.clj
+++
@@ -1,8 +0,0 @@
-(ns re-core.test.provider
-  (:require
-   [re-core.provider :refer (mappings)])
-  (:use midje.sweet))
-
-(fact "mappings"
-      (mappings {:os :ubuntu :domain "local"} {:os #{:template :flavor} :domain :search}) =>
-      {:template :ubuntu :flavor :ubuntu :search "local"})
