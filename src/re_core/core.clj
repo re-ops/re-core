6f0b71cc932bf288c6bcf1a9d04398d9accfe6ac
ronen <narkisr@gmail.com>
2018-09-10 21:18:58 +1000

    cleanup non used protocols

--- src/re_core/core.clj
+++ src/re_core/core.clj
@@ -8,13 +8,3 @@
   (stop [this]  "Stops a VM only if it exists and running")
   (status [this] "Returns vm status (values defere between providers) false if it does not exists")
   (ip [this] "Instance IP address"))
-
-(defprotocol Provision
-  "A provisioner (puppet/chef) base API, implement this to add more provisioners"
-  (apply- [this] "applies provisioner"))
-
-(defprotocol Remoter
-  "Remote automation (capistrano, fabric and re-mote) base api"
-  (setup [this] "Sets up this remoter (pulling code, etc..)")
-  (run [this] "executes a task on remote hosts with")
-  (cleanup [this] "Cleans up (deletes local source etc..)"))
