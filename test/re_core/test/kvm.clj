9c3c2c883c824a056c1b2bce733e1618d3f817da
ronen <narkisr@gmail.com>
2019-01-15 22:18:34 +1100

    test fix

--- test/re_core/test/kvm.clj
+++ test/re_core/test/kvm.clj
@@ -1,24 +1,22 @@
 (ns re-core.test.kvm
   (:require
+   [re-share.config :as conf]
    [re-core.fixtures.data :refer [redis-kvm volume]]
    [re-core.model :refer (vconstruct)])
   (:use clojure.test))
 
 (def expected-domain
-  {:user "re-ops" :name "red1" :hostname "red1"
-   :image {:flavor :debian :template "ubuntu-18.04"}
-   :cpu 2 :ram 1024})
+  {:user "re-ops" :name "red1" :hostname "red1" :cpu 4 :ram 1024
+   :image {:flavor :debian :template "ubuntu-server-18.04.1"}})
 
 (deftest kvm-sanity
   (testing "legal instance spec"
-    (with-redefs [re-core.model/env :dev]
-      (let [domain (vconstruct (assoc redis-kvm :system-id "1"))]
-        (is (= "1" (:system-id domain)))
-        (is (= {:user "ronen" :host "localhost" :port 22} (:node domain)))
-        (is (= expected-domain (:domain domain))))))
+    (let [domain (vconstruct (assoc redis-kvm :system-id "1"))]
+      (is (= "1" (:system-id domain)))
+      (is (= {:user "ronen" :name :localhost :host "localhost" :port 22} (:node domain)))
+      (is (= expected-domain (:domain domain)))))
   (testing "volume pool"
-    (with-redefs [re-core.model/env :dev]
-      (let [with-vol (assoc-in redis-kvm [:kvm :volumes] [volume])
-            domain (vconstruct (assoc with-vol :system-id "1"))]
-        (is (= (assoc volume :pool {:id "default" :path "/var/lib/libvirt/images/"})
-               (first (:volumes domain))))))))
+    (let [with-vol (assoc-in redis-kvm [:kvm :volumes] [volume])
+          domain (vconstruct (assoc with-vol :system-id "1"))]
+      (is (= (assoc volume :pool {:id "default" :path "/var/lib/libvirt/images/"})
+             (first (:volumes domain)))))))
