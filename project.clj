079bbc3bf9bb9e0efebfd9602b6642eb077882d6
ronen <narkisr.com>
2019-01-30 08:48:46 +1100

    bump

--- project.clj
+++ project.clj
@@ -26,7 +26,7 @@
                  [com.taoensso/tufte "1.1.1"]
 
                  ; re-ops
-                 [re-mote "0.10.1"]
+                 [re-mote "0.10.2"]
                  [re-share "0.9.13"]
 
                  ; Elasticsearc
