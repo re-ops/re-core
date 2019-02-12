a7e44ca7048fe8e82d58d01580ed2355a91a6c0d
ronen <narkisr.com>
2019-02-13 00:16:50 +1100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-core "0.11.0"
+(defproject re-core "0.11.1"
   :description "A launching pad for virtualized applications"
   :url "https://github.com/re-core-ops/re-core-core"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
