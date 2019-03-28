e05557a86aa2fb56446585cf67ed8937fc9256d6
ronen <narkisr.com>
2019-03-28 18:13:10 +1100

    lxc creation and listing works and adding configuration section

--- src/lxc/client.clj
+++ src/lxc/client.clj
@@ -1,6 +1,9 @@
 (ns lxc.client
   "lxc http client"
   (:require
+   [clojure.data.json :as json]
+   [re-share.config :refer (get!)]
+   [re-core.model :refer (hypervisor)]
    [less.awful.ssl :refer (ssl-context->engine ssl-p12-context)]
    [clojure.core.strint :refer (<<)]
    [org.httpkit.client :as http]))
@@ -9,35 +12,48 @@
 ; openssl pkcs12 -export -out certificate.p12 -inkey client.key -in client.crt -certfile servercerts/127.0.0.1.cr
 
 
-(def context
-  (less.awful.ssl/ssl-p12-context
-   "certificate.p12"
-   (char-array "")
-   "127.0.0.1.crt"))
+(defn context [{:keys [path p12 password crt]}]
+  (ssl-p12-context (<< "~{path}/~{p12}") (char-array password) (<< "~{path}/servercerts/~{crt}")))
 
-(def opts
-  {:sslengine (ssl-context->engine context)})
+(defn ssl-opts [node]
+  {:sslengine (ssl-context->engine (context node))})
+
+(defn into-body [m]
+  {:body (json/write-str m) :headers {"Content-Type" "application/json"}})
+
+(defn parse [response]
+  (let [{:keys [status body] :as result} (deref response)]
+    (if (and (>= status 400) (<= status 599))
+      (throw (ex-info "failed to process request" result))
+      (json/read-str body :key-fn keyword))))
+
+(defn run
+  ([endpoint node]
+   (run endpoint node {}))
+  ([endpoint node opts]
+   (parse (endpoint (merge (ssl-opts node) opts)))))
 
 (defn list
-  "list containers in lxd instance"
+  "List containers in lxd instance"
   [{:keys [host port] :as node}]
-  (let [{:keys [status body error]} @(http/get (<< "https://~{host}:~{port}/1.0/containers") opts)]
-    (if error
-      error
-      body)))
+  (run (partial http/get (<< "https://~{host}:~{port}/1.0/containers")) node))
 
 (defn create
   "Create container using http api"
-  [m]
-  ())
+  [{:keys [host port] :as node} container]
+  (run (partial http/post (<< "https://~{host}:~{port}/1.0/containers")) node (into-body container)))
 
 (comment
-
-  (list {:host "127.0.0.1" :port "8443"})
+  (def node
+    (merge {:host "127.0.0.1" :port "8443"} (hypervisor :lxc :auth)))
 
   (def m  {:name "my-new-container"
            :architecture "x86_64"
            :profiles ["default"]
+           :devices {}
            :ephemeral false
            :config {:limits.cpu "2"}
-           :source {:type "image" :alias "ubuntu:18:04"}}))
+           :source {:type "image" :alias "ubuntu-18.04"}})
+
+  (list node)
+  (create node m))
