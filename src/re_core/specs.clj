cac42d7abf656b546cdc79fb1f38b12a8788d797
ronen <narkisr@gmail.com>
2019-04-30 09:30:51 +1000

    tidying specs ns order, adding physical system and test

--- src/re_core/specs.clj
+++ src/re_core/specs.clj
@@ -5,6 +5,7 @@
    [re-core.presets.instance-types :as types]
    [re-core.model :refer (figure-virt)]))
 
+; Properties
 (def digital-regions #{"nyc1" "nyc2" "nyc3" "tor1" "sfo1" "sfo2" "sgp1" "lon1"})
 
 (def droplet-sizes #{"s-1vcpu-1gb" "s-1vcpu-2gb" "s-1vcpu-3gb" "s-2vcpu-2gb" "512mb"})
@@ -17,21 +18,16 @@
 
 (s/def ::node (s/and keyword? #(re-matches #"\w+" (name %))))
 
-(s/def ::lxc (s/keys :req-un [::node]))
+; Networking properties
+(def ethernet-address #"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$")
 
-(s/def ::kvm (s/keys :req-un [::node]))
+(s/def ::mac (s/and string? #(re-matches ethernet-address %)))
 
-(s/def ::digital-ocean (s/keys :req-un [:digital/region :digital/size :digital/private_networking]))
+(def ip #"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
 
-(defmulti system (fn [spec] (figure-virt spec)))
+(s/def ::ip (s/and string? #(re-matches ip %)))
 
-(defmethod system :lxc [_] (s/keys :req-un [:resource/machine ::lxc]))
-
-(defmethod system :kvm [_] (s/keys :req-un [:resource/machine ::kvm]))
-
-(defmethod system :digital-ocean [_] (s/keys :req-un [::digital-ocean]))
-
-(def ip #"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+(s/def ::broadcast (s/and string? #(re-matches ip %)))
 
 (def hostname-regex #"([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])*")
 
@@ -43,6 +39,7 @@
 
 (def user-regex #"^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$")
 
+; Other properties
 (s/def ::user (s/and string? #(re-matches user-regex %)))
 
 (def os-regex #"^[a-z]+\-[1-9]{1,2}\.[0-9]{1,2}[\.0-9]{0,2}$")
@@ -57,30 +54,31 @@
 
 (s/def ::ram (s/and int? valid-ram))
 
-(s/def :common/machine (s/keys :req-un [::hostname ::domain ::user ::os]))
+; Hypervisors
+(s/def ::lxc (s/keys :req-un [::node]))
+
+(s/def ::kvm (s/keys :req-un [::node]))
+
+(s/def ::digital-ocean (s/keys :req-un [:digital/region :digital/size :digital/private_networking]))
+
+(s/def ::physical (s/keys :req-un [::mac ::broadcast]))
+
+(defmulti system (fn [spec] (figure-virt spec)))
+
+(defmethod system :lxc [_] (s/keys :req-un [:resource/machine ::lxc]))
+
+(defmethod system :physical [_] (s/keys :req-un [::physical]))
+
+(defmethod system :kvm [_] (s/keys :req-un [:resource/machine ::kvm]))
+
+(defmethod system :digital-ocean [_] (s/keys :req-un [::digital-ocean]))
+
+; Common and main specs
+(s/def :common/machine (s/keys :req-un [::hostname ::domain ::user ::os] :opt-un [::ip]))
 
 (s/def :resource/machine (s/keys :req-un [::os ::cpu ::ram]))
 
 (s/def ::system (s/merge (s/multi-spec system figure-virt) (s/keys :req-un [:common/machine])))
 
 (comment
-  (def redis-lxc
-    {:machine {:hostname "red1" :user "root" :domain "local"
-               :os :ubuntu-18.04.2 :cpu 4 :ram 1}
-     :lxc {:node :localhost}
-     :type "redis"})
-
-  (def redis-kvm
-    {:machine {:hostname "red1" :user "re-ops" :domain "local"
-               :os :ubuntu-18.04.2 :cpu 4 :ram 1}
-     :kvm {:node :localhost}
-     :type "redis"})
-
-  (def redis-digital
-    {:machine {:hostname "red1" :user "root"
-               :domain "local" :os :ubuntu-18.04.2}
-     :digital-ocean {:region "lon1" :size "512mb"
-                     :private_networking false}
-     :type "redis"})
-
-  (s/explain ::system redis-digital))
+  (s/valid? ::system redis-physical))
