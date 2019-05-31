cf7b9265d094a63309ad47c1de06e4899b985226
ronen <narkisr@gmail.com>
2019-05-31 23:51:12 +1000

    moving to common lxc specs

--- src/lxc/spec.clj
+++ src/lxc/spec.clj
@@ -3,14 +3,6 @@
    [clojure.spec.alpha :as s]
    [re-share.spec :as re-ops]))
 
-(def p12-reg #"\\w+.p12")
-
-(s/def :lxc/p12 #(re-matches p12-reg %))
-
-(def crt-reg #"\\w+.crt")
-
-(s/def :lxc/crt #(re-matches crt-reg %))
-
 (s/def :lxc/node
   (s/keys :re-un [:re-ops/host :re-ops/port :re-ops/path :lxc/p12 :re-ops/password :lxc/crt]))
 
