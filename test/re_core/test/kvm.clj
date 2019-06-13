d6d55462357443f4ca2efb912671727348682754
ronen <narkisr@gmail.com>
2019-06-14 00:02:49 +1000

    fixing the test

--- test/re_core/test/kvm.clj
+++ test/re_core/test/kvm.clj
@@ -13,7 +13,7 @@
     (let [domain (vconstruct (assoc redis-kvm :system-id "1"))]
       (is (= "1" (:system-id domain)))
       (is (= {:user "ronen" :name :localhost :host "localhost" :port 22} (:node domain)))
-      (is (= expected-domain (:domain domain)))))
+      (is (= expected-domain (dissoc (:domain domain) :description)))))
   (testing "volume pool"
     (let [with-vol (assoc-in redis-kvm [:kvm :volumes] [volume])
           domain (vconstruct (assoc with-vol :system-id "1"))]
