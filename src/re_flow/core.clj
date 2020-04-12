048ea2e5bb67f156f956962cea08c59205076992
ronen <narkisr@gmail.com>
2020-04-13 01:49:55 +1000

    Adding restore flow

---
+++ src/re_flow/core.clj
@@ -0,0 +1,36 @@
+(ns re-flow.core
+  (:require
+   re-flow.setup
+   re-flow.restore
+   [re-share.core :refer (error-m)]
+   [taoensso.timbre :refer (refer-timbre)]
+   [clara.rules :refer :all]))
+
+(refer-timbre)
+
+(defquery get-failures
+  "Find all failures"
+  []
+  [?f <- ::state (= true (this :failure))])
+
+(defquery get-provisioned
+  "Find provisioned hosts"
+  []
+  [?p <- :re-flow.setup/provisioned])
+
+(def session (atom (mk-session 're-flow.core 're-flow.setup 're-flow.restore :fact-type-fn :state)))
+
+(defn run [& facts]
+  (future
+    (info "Starting to insert facts")
+    (try
+      (when-let [initialized-session (reduce (fn [s fact] (insert s fact)) @session facts)]
+        (reset! session (fire-rules initialized-session))
+        (info "Finished firing rules"))
+      (catch Exception e
+        (error-m e)))))
+
+(comment
+  (run {:state :re-flow.restore/start :flow :re-flow.restore/restore})
+  (query @session get-provisioned)
+  (query @session get-failures))
