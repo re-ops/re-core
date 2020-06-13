ea5464961326478ad5fdd6ca090f1bfbd94302c4
ronen <narkisr@gmail.com>
2020-06-13 23:26:38 +1000

    Latest versions of cog and re-cipe

--- project.clj
+++ project.clj
@@ -21,8 +21,8 @@
 
                  ; re-ops
                  [re-share "0.15.0"]
-                 [re-cog "0.4.10"]
-                 [re-cipes "0.1.5"]
+                 [re-cog "0.4.11"]
+                 [re-cipes "0.1.6"]
                  [re-scan "0.2.1"]
 
                  ; Elasticsearch
