ea3e22ce24c50add8bc92c63fb28457e36b2cd0e
ronen <narkisr@gmail.com>
2020-06-14 21:54:53 +1000

    Cog version up

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
 
                  ; re-ops
                  [re-share "0.15.0"]
-                 [re-cog "0.4.11"]
+                 [re-cog "0.4.12"]
                  [re-cipes "0.1.6"]
                  [re-scan "0.2.1"]
 
