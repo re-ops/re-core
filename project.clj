adad50e66ed0a69221f4c8cb688f3c99b59aeaea
ronen <narkisr@gmail.com>
2020-07-29 23:23:02 +1000

    Latest share

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
                  [org.clojure/tools.trace "0.7.9"]
 
                  ; re-ops
-                 [re-share "0.16.1"]
+                 [re-share "0.16.2"]
                  [re-cog "0.4.17"]
                  [re-cipes "0.1.26"]
                  [re-scan "0.2.1"]
