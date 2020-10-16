2d54977f794a58802fb93baf8bac0460239270ff
ronen <narkisr@gmail.com>
2020-10-17 01:06:45 +1100

    Latest re-cipes

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                  ; re-ops
                  [re-share "0.16.7"]
                  [re-cog "0.5.8"]
-                 [re-cipes "0.2.2"]
+                 [re-cipes "0.2.5"]
                  [re-scan "0.2.1"]
 
                  ; Elasticsearch
