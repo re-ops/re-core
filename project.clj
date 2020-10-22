e7bc500fea192953ad631db532eed6a90ff7c069
ronen <narkisr@gmail.com>
2020-10-23 00:56:39 +1100

    Moving run internally

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
 
                  ; re-ops
                  [re-share "0.16.7"]
-                 [re-cog "0.5.10"]
+                 [re-cog "0.5.11"]
                  [re-cipes "0.2.6"]
                  [re-scan "0.2.1"]
 
