f109fd15790ae76726cb7e48b29a51b4cd4f8e61
ronen <narkisr@gmail.com>
2020-11-08 01:17:28 +1100

    Latest cog fixing the build

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
 
                  ; re-ops
                  [re-share "0.16.7"]
-                 [re-cog "0.5.14"]
+                 [re-cog "0.5.15"]
                  [re-cipes "0.2.8"]
                  [re-scan "0.2.1"]
 
