dd889c778c0dd0908e3900773086eafe393929c3
ronen <narkisr@gmail.com>
2020-11-12 01:05:23 +1100

    Creating multiple files

--- test/re_flow/integration/certs.clj
+++ test/re_flow/integration/certs.clj
@@ -8,7 +8,7 @@
    [re-core.presets.systems :refer (kvm defaults local)]
    [re-core.presets.instance-types :refer (large)]
    [re-flow.core :refer (trigger)]
-   [re-flow.session :refer (fact-type session)]
+   [re-flow.session :refer (fact-type session update-)]
    [taoensso.timbre :refer (info)]
    [clara.rules :refer :all]
    [re-flow.common :refer (run-?e failure? successful-ids hosts-results*)]
@@ -23,12 +23,16 @@
 
 (def fact (promise))
 
+(defn create-file [ids f]
+  (hosts-results* (run-hosts (hosts (with-ids ids) :hostname) file [f :present] [1 :second])))
+
 (defrule domains
   "Setup the domains we will generate certs for"
   [?e <- :re-flow.setup/provisioned [{:keys [failure]}] (= failure false)]
+  [?f <- :re-flow.integration/files]
   =>
   (let [{:keys [ids]} ?e
-        r (hosts-results* (run-hosts (hosts (with-ids ids) :hostname) file ["/tmp/1" :present] [1 :second]))]
+        r (mapv (partial create-file ids) (?f :files))]
     (deliver fact r)))
 
 (defn setup
@@ -38,8 +42,9 @@
   (f))
 
 (deftest cert-distribution
+  (update- [{:type :re-flow.integration/files :files ["/tmp/1" "/tmp/2"]}])
   (trigger (create-fact kvm defaults local large :letsencrypt "cert generation"))
   (let [r (deref fact)]
-    (info "got ready system" r)))
+    (info "got result back" r)))
 
 (use-fixtures :once setup)
