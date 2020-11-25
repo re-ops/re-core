4756142b085cc620d868ab85b1fedd3dc97e2aaa
ronen <narkisr@gmail.com>
2020-11-25 21:26:25 +1100

    Using hostnamectl

--- src/hypervisors/networking.clj
+++ src/hypervisors/networking.clj
@@ -3,19 +3,13 @@
   (:require
    [taoensso.timbre :refer (refer-timbre)]
    [re-mote.ssh.transport :refer (execute)]
-   [re-cog.scripts.hostname :refer (kernel-hostname override-hostname redhat-hostname)]
-   [clojure.core.strint :refer [<<]]))
+   [re-cog.scripts.hostname :refer (hostnamectl)]))
 
 (refer-timbre)
 
 (defn set-hostname
   [hostname fqdn remote flavor]
-  (execute (kernel-hostname hostname fqdn) remote)
-  (execute (override-hostname hostname fqdn) remote)
-  (case flavor
-    :debian  true ; nothing special todo
-    :redhat (execute (redhat-hostname fqdn) remote)
-    (throw (ex-info (<< "no os flavor found for ~{flavor}") {:flavor flavor}))))
+  (execute (hostnamectl hostname fqdn) remote))
 
 (defn ssh-able? [flavor]
   (#{:redhat :debian} flavor))
