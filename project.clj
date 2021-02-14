8496cbfac6a68d08f8d7f672532f83c3d2329ae3
ronen <narkisr@gmail.com>
2021-02-14 16:29:07 +1100

    Latest re-cipes

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
                  ; re-ops
                  [re-share "0.17.0"]
                  [re-cog "0.5.22"]
-                 [re-cipes "0.3.5"]
+                 [re-cipes "0.3.7"]
                  [re-scan "0.2.1"]
 
                  ; Elasticsearch
