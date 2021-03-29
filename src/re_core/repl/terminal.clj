7d8a7b7081fa3a945730037d5d5c3398de240c2b
ronen <narkisr@gmail.com>
2021-03-29 22:58:17 +1100

    Adding doc and future (for mate-terminal)

--- src/re_core/repl/terminal.clj
+++ src/re_core/repl/terminal.clj
@@ -4,8 +4,11 @@
    [clojure.core.strint :refer  (<<)]
    [clojure.java.shell :refer (sh)]))
 
-(defn launch-ssh [target private-key]
+(defn launch-ssh
+  "Make sure to set the default terminal:
+       sudo update-alternatives --config x-terminal-emulator"
+  [target private-key]
   (if (System/getenv "SSH_CONNECTION")
     (sh "tmux" "split-window" (<< "/usr/bin/ssh ~{target} -i ~{private-key}"))
-    (sh "/usr/bin/x-terminal-emulator" "--disable-factory" "-e" (<< "/usr/bin/ssh ~{target} -i ~{private-key}"))))
+    (future (sh "/usr/bin/x-terminal-emulator" "--disable-factory" "-e" (<< "/usr/bin/ssh ~{target} -i ~{private-key}")))))
 
