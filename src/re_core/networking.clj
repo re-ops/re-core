a6bdf47d0ba704fd456ca60ebe32174b8668de83
ronen <narkisr@gmail.com>
2021-07-18 17:49:37 +1000

    Fixing system listing

--- src/re_core/networking.clj
+++ src/re_core/networking.clj
@@ -1,16 +1,22 @@
 (ns re-core.networking
   "Common systems networking logic"
+  (:refer-clojure :exclude [list])
   (:require
-   [re-core.repl :refer (with-ids)]
+   [taoensso.timbre :refer (refer-timbre)]
+   [re-core.repl :refer (with-ids list)]
    [re-share.wait :refer (wait-for)])
   (:import clojure.lang.ExceptionInfo))
 
+(refer-timbre)
+
 (defn ips-available [ids]
   (try
-    (wait-for {:timeout [1 :minute]}
+    (wait-for {:timeout [1 :minute] :sleep [1000 :ms]}
               (fn []
                 (let [systems (-> (list (with-ids ids) :systems :print? false) second :systems)]
-                  (every? (comp not nil? :ip :machine second) systems)))
+                  (and
+                   (not (empty? systems))
+                   (every? (comp not nil? :ip :machine second) systems))))
               "Failed to wait for ips to become available")
     true
     (catch ExceptionInfo _ false)))
