f170346325abbc43f60918c01745539282c175af
ronen <narkisr@gmail.com>
2021-09-14 01:18:41 +1000

    latest re-cipes

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                  ; re-ops
                  [re-share "0.18.0"]
                  [re-cog "0.6.6"]
-                 [re-cipes "0.3.13"]
+                 [re-cipes "0.3.14"]
                  [re-scan "0.2.1"]
 
                  ; Elasticsearch
