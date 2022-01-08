295b527d9e246ffd08acc6dd80d83c3a25dee7aa
ronen <narkisr@gmail.com>
2022-01-08 23:56:54 +1100

    Fixing vector flattening and un-flattening

--- src/re_core/persistency/common.clj
+++ src/re_core/persistency/common.clj
@@ -1,8 +1,10 @@
 (ns re-core.persistency.common
   "common persistency layer functions"
   (:require
-   [re-cog.facts.datalog :refer (flatten-keys join-keys fact-pairs)]
-   [clojure.string :refer (escape)]))
+   [clojure.walk :refer [postwalk]]
+   [com.rpl.specter :refer (transform select ALL)]
+   [re-cog.facts.datalog :refer (flatten-keys fact-pairs)]
+   [clojure.string :refer (escape join)]))
 
 (defn args-of
   "grab args from string"
@@ -15,11 +17,25 @@
     (map? a2) (apply f a1 (t a2) r)
     :else (apply f args)))
 
+(defn into-vec [m]
+  (mapv m (sort (keys m))))
+
+(defn vec-map? [m]
+  (every? (partial re-find #"\d+") (map name (keys m))))
+
+(defn restore-vectors [m]
+  (postwalk
+   (fn [v]
+     (if (and (map? v) (vec-map? v)) (into-vec v) v)) m))
+
 (defn unflatten [m]
-  (reduce
-   (fn [m [k v]]
-     (assoc-in m (mapv keyword (clojure.string/split (subs (str k) 1) #"\/")) v)) {} m))
+  (restore-vectors
+   (reduce
+    (fn [m [k v]]
+      (assoc-in m (mapv keyword (clojure.string/split (subs (str k) 1) #"\/")) v)) {} m)))
+
+(defn join-keys [[ks v]]
+  [(keyword (join "/" (map (fn [k] (if (keyword? k) (name k) (str k))) ks))) v])
 
 (defn flatten- [m]
-  (into {} (map first (map join-keys (flatten-keys m)))))
-
+  (into {} (map join-keys (flatten-keys m))))
