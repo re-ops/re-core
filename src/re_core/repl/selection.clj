0f5c9ddc1e006396edfee1f0d5dc9d8b39ba5fe3
ronen <narkisr@gmail.com>
2022-05-14 17:51:55 +1000

    Adding physical registered hosts

--- src/re_core/repl/selection.clj
+++ src/re_core/repl/selection.clj
@@ -1,11 +1,22 @@
 (ns re-core.repl.selection
   "comon selected hosts presets"
   (:require
-   [re-core.repl :refer (hosts match-kv named)]
+   [re-core.repl :refer (hosts match-kv named hyp ip)]
    [re-mote.zero.management :refer (all-hosts)]))
 
-(defn all-instances []
+(defn all-instances
+  "All hosts which aren't marked as disabled"
+  []
   (hosts (match-kv [:disabled] nil) :hostname))
 
-(defn registered-instances []
+(defn registered-instances
+  "Hosts which have a hostname registered"
+  []
   (hosts (named (keys (all-hosts))) :hostname))
+
+(defn physical-registered-hosts
+  "Physical hosts which are also registered"
+  []
+  (hosts
+   (fn [[v m]]
+     (and ((named (keys (all-hosts))) [v m]) ((hyp :physical) [v m]) (ip [v m]))) :hostname))
